b48c60028222e0383da34f8af1a586553123ffb5
Hernando <hernandoabella@gmail.com>
2020-09-18 18:55:33 -0400

    'hi'

---
+++ hola mundo/holamundo.js
@@ -0,0 +1,5 @@
+let saludo; // Creamos una variable saludo.
+
+saludo = document.getElementById('saludo'); // Referenciamos el elemento HTML
+
+saludo.innerHTML = 'Hola yes!'; // Cambiamos el texto actual del elemento y lo remplazamos por el texto Hola Mundo
