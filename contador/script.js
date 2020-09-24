de7496cbfa5084d5e6b16fcf3a1ef3204e78af7e
Hernando <hernandoabella@gmail.com>
2020-09-23 22:41:23 -0400

    changes maked

---
+++ contador/script.js
@@ -0,0 +1,21 @@
+//variables
+
+let numero = document.getElementById('numero');
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+
+const sumar = () => {
+   return numero.innerText++;
+}
+
+
+const restar = () => {
+   return numero.innerText--;
+}
+
+
+//activar contador automático: 
+//setInterval(sumar, 500);
+
+
+
