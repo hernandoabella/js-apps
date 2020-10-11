131925ba89bf95c50db032cb74d82ee759ffdef9
Hernando <hernandoabella@gmail.com>
2020-10-11 16:40:05 -0400

    adding program

--- convierte grados celsius a fahrenheit/script.js
+++ convierte grados celsius a fahrenheit/script.js
@@ -4,7 +4,8 @@ const convertir = () =>{
     let celsius = document.getElementById('celsius').value;
     let fahrentheit = (celsius * 1.8) + 32;
     resultado.style.display = 'block';
-    let cadenaDeTexto = `${celsius} grados celsius es igual a 
-    ${fahrentheit} grados Fahrenheit`;
+    let cadenaDeTexto = `${celsius} grados celsius son iguales a 
+    ${fahrentheit} grados Fahrenheit.`;
     return resultado.innerHTML = cadenaDeTexto;
 }
+
