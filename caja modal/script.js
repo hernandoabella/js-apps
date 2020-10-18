66d8762cd0079a592d9bc16d0295a848b2da0aec
Hernando <hernandoabella@gmail.com>
2020-10-17 22:29:36 -0400

    carrito de compras

--- caja modal/script.js
+++ caja modal/script.js
@@ -1,153 +1,26 @@
-window.onload = function () {
-    // Variables
-    let baseDeDatos = [
-        {
-            id: 1,
-            nombre: 'Patata',
-            precio: 1,
-            imagen: 'https://source.unsplash.com/random/500x500/?potato&sig=1'
-        },
-        {
-            id: 2,
-            nombre: 'Cebolla',
-            precio: 1.2,
-            imagen: 'https://source.unsplash.com/random/500x500/?onion&sig=2'
-        },
-        {
-            id: 3,
-            nombre: 'Calabacin',
-            precio: 2.1,
-            imagen: 'https://source.unsplash.com/random/500x500/?zucchini&sig=3'
-        }
-    ];
+// selecciona el modal
+let modal = document.getElementById("myModal");
 
-    let $items = document.querySelector('#items');
-    let carrito = [];
-    let total = 0;
-    let $carrito = document.querySelector('#carrito');
-    let $total = document.querySelector('#total');
-    let $botonVaciar = document.querySelector('#boton-vaciar');
+// Selecciona el botón que abre el modal
+let btn = document.getElementById("myBtn");
 
-    // Funciones
-    function renderItems() {
-        for (let info of baseDeDatos) {
-            // Estructura
-            let miNodo = document.createElement('div');
-            miNodo.classList.add('card', 'col-sm-4');
-            // Body
-            let miNodoCardBody = document.createElement('div');
-            miNodoCardBody.classList.add('card-body');
-            // Titulo
-            let miNodoTitle = document.createElement('h5');
-            miNodoTitle.classList.add('card-title');
-            miNodoTitle.textContent = info['nombre'];
-            // Imagen
-            let miNodoImagen = document.createElement('img');
-            miNodoImagen.classList.add('img-fluid');
-            miNodoImagen.setAttribute('src', info['imagen']);
-            // Precio
-            let miNodoPrecio = document.createElement('p');
-            miNodoPrecio.classList.add('card-text');
-            miNodoPrecio.textContent = info['precio'] + '€';
-            // Boton 
-            let miNodoBoton = document.createElement('button');
-            miNodoBoton.classList.add('btn', 'btn-primary');
-            miNodoBoton.textContent = '+';
-            miNodoBoton.setAttribute('marcador', info['id']);
-            miNodoBoton.addEventListener('click', anyadirCarrito);
-            // Insertamos
-            miNodoCardBody.appendChild(miNodoImagen);
-            miNodoCardBody.appendChild(miNodoTitle);
-            miNodoCardBody.appendChild(miNodoPrecio);
-            miNodoCardBody.appendChild(miNodoBoton);
-            miNodo.appendChild(miNodoCardBody);
-            $items.appendChild(miNodo);
-        }
-    }
+// Selecciona el elemento close que cierra el modal
+let span = document.getElementsByClassName("close")[0];
 
-    function anyadirCarrito () {
-        // Añadimos el Nodo a nuestro carrito
-        carrito.push(this.getAttribute('marcador'))
-        // Calculo el total
-        calcularTotal();
-        // Renderizamos el carrito 
-        renderizarCarrito();
-    }
+// Abre el modal cuando el usuario presiona el botón
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
-    function renderizarCarrito() {
-        // Vaciamos todo el html
-        $carrito.textContent = '';
-        // Quitamos los duplicados
-        let carritoSinDuplicados = [...new Set(carrito)];
-        // Generamos los Nodos a partir de carrito
-        carritoSinDuplicados.forEach(function (item, indice) {
-            // Obtenemos el item que necesitamos de la variable base de datos
-            let miItem = baseDeDatos.filter(function(itemBaseDatos) {
-                return itemBaseDatos['id'] == item;
-            });
-            // Cuenta el número de veces que se repite el producto
-            let numeroUnidadesItem = carrito.reduce(function (total, itemId) {
-                return itemId === item ? total += 1 : total;
-            }, 0);
-            // Creamos el nodo del item del carrito
-            let miNodo = document.createElement('li');
-            miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
-            miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0]['nombre']} - ${miItem[0]['precio']}€`;
-            // Boton de borrar
-            let miBoton = document.createElement('button');
-            miBoton.classList.add('btn', 'btn-danger', 'mx-5');
-            miBoton.textContent = 'X';
-            miBoton.style.marginLeft = '1rem';
-            miBoton.setAttribute('item', item);
-            miBoton.addEventListener('click', borrarItemCarrito);
-            // Mezclamos nodos
-            miNodo.appendChild(miBoton);
-            $carrito.appendChild(miNodo);
-        })
-    }
+// Cierra el modal cuando el usuario presiona la X
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
-    function borrarItemCarrito() {
-        console.log()
-        // Obtenemos el producto ID que hay en el boton pulsado
-        let id = this.getAttribute('item');
-        // Borramos todos los productos
-        carrito = carrito.filter(function (carritoId) {
-            return carritoId !== id;
-        });
-        // volvemos a renderizar
-        renderizarCarrito();
-        // Calculamos de nuevo el precio
-        calcularTotal();
-    }
+// Cierra el modal cuando el usuario hace clic afuera
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
-    function calcularTotal() {
-        // Limpiamos precio anterior
-        total = 0;
-        // Recorremos el array del carrito
-        for (let item of carrito) {
-            // De cada elemento obtenemos su precio
-            let miItem = baseDeDatos.filter(function(itemBaseDatos) {
-                return itemBaseDatos['id'] == item;
-            });
-            total = total + miItem[0]['precio'];
-        }
-        // Formateamos el total para que solo tenga dos decimales
-        let totalDosDecimales = total.toFixed(2);
-        // Renderizamos el precio en el HTML
-        $total.textContent = totalDosDecimales;
-    }
-
-    function vaciarCarrito() {
-        // Limpiamos los productos guardados
-        carrito = [];
-        // Renderizamos los cambios
-        renderizarCarrito();
-        calcularTotal();
-    }
-
-    // Eventos
-    $botonVaciar.addEventListener('click', vaciarCarrito);
-
-    // Inicio
-    renderItems();
-} 
--- carrito de compras/script.js
+++ carrito de compras/script.js
@@ -1,26 +1,169 @@
-// selecciona el modal
-let modal = document.getElementById("myModal");
+    // Variables
+    let baseDeDatos = [
+        {
+            id: 1,
+            nombre: 'Nintendo Switch',
+            precio: 299.00,
+            imagen: 'https://images-na.ssl-images-amazon.com/images/I/71ILSOE6ovL._AC_SL1500_.jpg'
+        },
+        {
+            id: 2,
+            nombre: 'ASUS VivoBook 15',
+            precio: 482.00,
+            imagen: 'https://images-na.ssl-images-amazon.com/images/I/81fstJkUlaL._AC_SL1500_.jpg'
+        },
+        {
+            id: 3,
+            nombre: 'AMD Ryzen 7',
+            precio: 294.99,
+            imagen: 'https://images-na.ssl-images-amazon.com/images/I/717Di3DGIbL._AC_SL1092_.jpg'
+        },
+        {
+            id: 4,
+            nombre: 'Acer Aspire C24',
+            precio: 579.99,
+            imagen: 'https://images-na.ssl-images-amazon.com/images/I/61PlVVOd3QL._AC_SL1500_.jpg'
+        },
+        {
+            id: 5,
+            nombre: 'Mouse Redragon M601',
+            precio: 15.99,
+            imagen: 'https://images-na.ssl-images-amazon.com/images/I/61okxuhfKBL._AC_SL1500_.jpg'
+        },
+        {
+            id: 6,
+            nombre: 'Teclado Mecánico',
+            precio: 46.92,
+            imagen: 'https://images-na.ssl-images-amazon.com/images/I/819bA8-GcWL._AC_SL1500_.jpg'
+        }
+    ];
 
-// Selecciona el botón que abre el modal
-let btn = document.getElementById("myBtn");
+    let $items = document.querySelector('#items');
+    let carrito = [];
+    let total = 0;
+    let $carrito = document.querySelector('#carrito');
+    let $total = document.querySelector('#total');
+    let $botonVaciar = document.querySelector('#boton-vaciar');
 
-// Selecciona el elemento close que cierra el modal
-let span = document.getElementsByClassName("close")[0];
+    // Funciones
+    function renderItems() {
+        for (let info of baseDeDatos) {
+            // Estructura
+            let miNodo = document.createElement('div');
+            miNodo.classList.add('tarjeta');
+            // Body
+            let miNodoCardBody = document.createElement('div');
+            miNodoCardBody.classList.add('cuerpoCarta');
+            // Titulo
+            let miNodoTitle = document.createElement('h5');
+            miNodoTitle.classList.add('tituloTarjeta');
+            miNodoTitle.textContent = info['nombre'];
+            // Imagen
+            let miNodoImagen = document.createElement('img');
+            miNodoImagen.classList.add('imagenTarjeta');
+            miNodoImagen.setAttribute('src', info['imagen']);
+            // Precio
+            let miNodoPrecio = document.createElement('p');
+            miNodoPrecio.classList.add('textoTarjeta');
+            miNodoPrecio.textContent = info['precio'] + '$';
+            // Boton 
+            let miNodoBoton = document.createElement('button');
+            miNodoBoton.classList.add('agregar');
+            miNodoBoton.textContent = '+';
+            miNodoBoton.setAttribute('marcador', info['id']);
+            miNodoBoton.addEventListener('click', anyadirCarrito);
+            // Insertamos
+            miNodoCardBody.appendChild(miNodoImagen);
+            miNodoCardBody.appendChild(miNodoTitle);
+            miNodoCardBody.appendChild(miNodoPrecio);
+            miNodoCardBody.appendChild(miNodoBoton);
+            miNodo.appendChild(miNodoCardBody);
+            $items.appendChild(miNodo);
+        }
+    }
 
-// Abre el modal cuando el usuario presiona el botón
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+    function anyadirCarrito () {
+        // Añadimos el Nodo a nuestro carrito
+        carrito.push(this.getAttribute('marcador'))
+        // Calculo el total
+        calcularTotal();
+        // Renderizamos el carrito 
+        renderizarCarrito();
+    }
 
-// Cierra el modal cuando el usuario presiona la X
-span.onclick = function() {
-  modal.style.display = "none";
-}
+    function renderizarCarrito() {
+        // Vaciamos todo el html
+        $carrito.textContent = '';
+        // Quitamos los duplicados
+        let carritoSinDuplicados = [...new Set(carrito)];
+        // Generamos los Nodos a partir de carrito
+        carritoSinDuplicados.forEach(function (item, indice) {
+            // Obtenemos el item que necesitamos de la variable base de datos
+            let miItem = baseDeDatos.filter(function(itemBaseDatos) {
+                return itemBaseDatos['id'] == item;
+            });
+            // Cuenta el número de veces que se repite el producto
+            let numeroUnidadesItem = carrito.reduce(function (total, itemId) {
+                return itemId === item ? total += 1 : total;
+            }, 0);
+            // Creamos el nodo del item del carrito
+            let miNodo = document.createElement('li');
+            miNodo.classList.add('listaCarrito');
+            miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0]['nombre']} - ${miItem[0]['precio']}$`;
+            // Boton de borrar
+            let miBoton = document.createElement('button');
+            miBoton.classList.add('cerrar');
+            miBoton.textContent = 'X';
+            miBoton.style.marginLeft = '1rem';
+            miBoton.setAttribute('item', item);
+            miBoton.addEventListener('click', borrarItemCarrito);
+            // Mezclamos nodos
+            miNodo.appendChild(miBoton);
+            $carrito.appendChild(miNodo);
+        })
+    }
 
-// Cierra el modal cuando el usuario hace clic afuera
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+    function borrarItemCarrito() {
+        console.log()
+        // Obtenemos el producto ID que hay en el boton pulsado
+        let id = this.getAttribute('item');
+        // Borramos todos los productos
+        carrito = carrito.filter(function (carritoId) {
+            return carritoId !== id;
+        });
+        // volvemos a renderizar
+        renderizarCarrito();
+        // Calculamos de nuevo el precio
+        calcularTotal();
+    }
 
+    function calcularTotal() {
+        // Limpiamos precio anterior
+        total = 0;
+        // Recorremos el array del carrito
+        for (let item of carrito) {
+            // De cada elemento obtenemos su precio
+            let miItem = baseDeDatos.filter(function(itemBaseDatos) {
+                return itemBaseDatos['id'] == item;
+            });
+            total = total + miItem[0]['precio'];
+        }
+        // Formateamos el total para que solo tenga dos decimales
+        let totalDosDecimales = total.toFixed(2);
+        // Renderizamos el precio en el HTML
+        $total.textContent = totalDosDecimales;
+    }
+
+    function vaciarCarrito() {
+        // Limpiamos los productos guardados
+        carrito = [];
+        // Renderizamos los cambios
+        renderizarCarrito();
+        calcularTotal();
+    }
+
+    // Eventos
+    $botonVaciar.addEventListener('click', vaciarCarrito);
+
+    // Inicio
+    renderItems();
