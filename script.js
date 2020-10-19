570bc1cc4f32f41a4c5cfd04c1100eb9556f74f7
Hernando <hernandoabella@gmail.com>
2020-10-19 19:27:04 -0400

    frontpage update

--- script.js
+++
@@ -1,4 +0,0 @@
-var array = ['Hola Mundo', 'Convertir de Horas a Segundos', 'Suma Dos Números', 'Contador', 'Reloj', 'Generador de Frases', 'Sección Colapsable', 'Cambiar Color de Fondo', 'Rotación de Cadena', 'Encuentra la Raíz', 'Calcula el Área de un Triángulo', 'Convertir Kilómetros a Millas', 'Convertir Grados Celsius a Farenheit', 'Encuentra el Factorial de un Número', 'Tabla de Multiplicación', 'Lista de Quehaceres', 'Caja Modal', 'Carrito de Compras', 'Calculador de Propinas']; 
-
-target.innerHTML = '<li>' + array.join('</li><li>') + '</li>';
-
