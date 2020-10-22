0ebefbceee21acf66e3625813d7628320c9461ce
Hernando <hernandoabella@gmail.com>
2020-10-22 19:53:22 -0400

    adding copy clic stuf

---
+++ 22. Generador de Contraseñas/script.js
@@ -0,0 +1,14 @@
+let listaClave = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%&*/\?";
+let temp = "";
+
+function generatePass(pLength) {
+    temp = "";
+    for (var i = 0; i < pLength; i++) {
+        temp += listaClave.charAt(Math.floor(Math.random() * listaClave.length))
+    }
+    return temp;
+}
+
+function populateForm(enterLength) {
+    document.generarPassword.salida.value = generatePass(enterLength);
+}
