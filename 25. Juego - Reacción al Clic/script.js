203db7074b1263e2905dc177b6806d83e843b049
Hernando <hernandoabella@gmail.com>
2020-10-27 12:05:21 -0400

    working state

--- 25. Juego - Reacción al Clic/script.js
+++ 25. Juego - Reacción al Clic/script.js
@@ -12,11 +12,11 @@ function showMessage(notification){
 }
 
 function showBox(){
-    playAreaArea.timer = setTimeout(myBox, random(4000));
+    playArea.timer = setTimeout(myBox, random(4000));
 }
 
 function myBox(){
-
+    //create a shape element
     let element = document.createElement('div');
     element.classList.add('box');
     element.style.top = random(setTopMargin()) + 'px';
@@ -27,35 +27,36 @@ function myBox(){
     gameArea.appendChild(element);
 }
 
+// Pick a random hex color
 function getColor(){
     function col(){
         let hex = random(255).toString(16);
-
+        //always return 2 values, even if a 0 is apended
         return ('0' + String(hex)).substr(-2);
     }
     return '#' + col() + col() + col();
 }
 
-
+ //Adjust top margin so circle is not on the edge
 function setTopMargin(){
     let maxHeight = gameArea.clientHeight;
-    if(maxHeight <= 100){
+    if (maxHeight <= 100){
         maxHeight = maxHeight + 200;
-    }else{
+    } else {
         maxHeight = maxHeight - 200;
     }
-
     return maxHeight;
 }
 
+//Adjust left margin so circle is not on the edge
 function setLeftMargin(){
     let maxWidth = gameArea.clientWidth;
-    if(maxWidth <= 100){
-        maWidth = maxWidth + 200;
-    }else{
+    if (maxWidth <= 100){
+        maxWidth = maxWidth + 200;
+    } else {
         maxWidth = maxWidth - 200;
     }
-
+    
     return maxWidth;
 }
 
@@ -64,26 +65,24 @@ function hit(e){
     let end = new Date().getTime();
     let duration = (end-start)/1000;
     let maxDuration = 1;
-
+    
     clearTimeout(playArea.timer);
     showMessage('It took you ' + duration + ' seconds to click');
-
-    if(duration > maxDuration){
+    if (duration > maxDuration){
         gameArea.children[0].remove();
         results.innerHTML = `Too Slow! <span id="loser">You Lose!</span> Your score was ${count}.<br> Click the start button to play again!`;
         resetGame();
-    }else{
+    } else {
         gameArea.children[0].remove();
         playArea.timer = setTimeout(myBox, random(4000));
         count++;
-        if(count === 15){
+        if (count === 15){
             results.innerHTML = `You reached ${renderCount(count)}! <span id="winner">You win!</span> <br> Click start to Play again.`;
             resetGame();
-        }else{
+        } else {
             results.innerHTML = `Score: ${renderCount(count)} of 15`;
         }
     }
-
 }
 
 function renderCount(count){
@@ -91,7 +90,7 @@ function renderCount(count){
 }
 
 function random(number){
-    let tempVal = Math.floor(Math.random() * number);
+    let tempVal = Math.floor(Math.random()*number);
     return tempVal;
 }
 
@@ -104,13 +103,14 @@ function resetGame(){
 showMessage('Click Start to Begin!');
 
 button.addEventListener('click', function(){
-    inPlay = true;
-
-    button.style.display = 'none';
-    directions.style.display = 'none';
-    results.innerHTML = '';
-    count = 0;
-
+    //start game play
+        inPlay = true;
+        //hide the button
+        button.style.display = 'none';
+        directions.style.display = 'none';
+        results.innerHTML = '';
+        count = 0;
+    //notify user of start
     showMessage('Starting...');
 
     showBox();
