0984cba0c9d4e193dd9b2edc974a3bbc6f887b96
Hernando <hernandoabella@gmail.com>
2020-10-27 12:01:16 -0400

    finish script

--- 25. Juego - Reacción al Clic/script.js
+++ 25. Juego - Reacción al Clic/script.js
@@ -75,6 +75,43 @@ function hit(e){
     }else{
         gameArea.children[0].remove();
         playArea.timer = setTimeout(myBox, random(4000));
+        count++;
+        if(count === 15){
+            results.innerHTML = `You reached ${renderCount(count)}! <span id="winner">You win!</span> <br> Click start to Play again.`;
+            resetGame();
+        }else{
+            results.innerHTML = `Score: ${renderCount(count)} of 15`;
+        }
     }
 
 }
+
+function renderCount(count){
+    return count;
+}
+
+function random(number){
+    let tempVal = Math.floor(Math.random() * number);
+    return tempVal;
+}
+
+function resetGame(){
+    clearTimeout(playArea.timer);
+    inPlay = false;
+    button.style.display = 'block';
+}
+
+showMessage('Click Start to Begin!');
+
+button.addEventListener('click', function(){
+    inPlay = true;
+
+    button.style.display = 'none';
+    directions.style.display = 'none';
+    results.innerHTML = '';
+    count = 0;
+
+    showMessage('Starting...');
+
+    showBox();
+})
