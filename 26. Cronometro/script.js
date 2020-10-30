67dd9701796c2c90e6b0fc88d25b37a487adc622
Hernando <hernandoabella@gmail.com>
2020-10-30 15:17:41 -0400

    new program added

--- 26. Cronometro/script.js
+++ 26. Cronometro/script.js
@@ -1,56 +1,59 @@
 window.onload = function(){
 
-    var seconds = 00;
-    var tens = 00;
-
+    var segundos = 00;
+    var decenas = 00;
     var appendTens = document.getElementById("tens");
     var appendSeconds = document.getElementById("seconds");
-    var buttonStart = this.document.getElementById("button-start");
-    var buttonStop = this.document.getElementById("button-stop");
-    var buttonReset = this.document.getElementById("button-reset");
+    var botonEmpezar = this.document.getElementById("button-start");
+    var botonDetener = this.document.getElementById("button-stop");
+    var botonReiniciar = this.document.getElementById("button-reset");
 
     var Interval;
 
-                /*First Step*/
-    /*first function that start timer working*/
-    function startTimer(){
-        tens++;
-        //console.log(tens);
-        if(tens < 9){
-            appendTens.innerHTML = "0" + tens;
+               
+    function iniciarTemporizador(){
+		decenas++;
+		
+        if(decenas < 9){
+            appendTens.innerHTML = "0" + decenas;
+		}
+		
+        if(decenas > 9){
+            appendTens.innerHTML = decenas;
+		}
+		
+        if(decenas > 99){
+            segundos++;
+            console.log(segundos);
+            appendSeconds.innerHTML = "0" + segundos;
+            decenas = 0;
+            appendTens.innerHTML = "0" + decenas;
+		}
+		
+        if(segundos > 9){
+            appendSeconds.innerHTML = segundos;
         }
-        if(tens > 9){
-            appendTens.innerHTML = tens;
-        }
-        if(tens > 99){
-            seconds++;
-            console.log(seconds);
-            appendSeconds.innerHTML = "0" + seconds;
-            tens = 0;
-            appendTens.innerHTML = "0" + tens;
-        }
-        if(seconds > 9){
-            appendSeconds.innerHTML = seconds;
-        }
-    }
-    /*Button to Start Timer*/
-    buttonStart.onclick = function(){
+	}
+	
+    /*Button para empezar*/
+    botonEmpezar.onclick = function(){
         clearInterval(Interval);
-        Interval = setInterval(startTimer, 10);
+        Interval = setInterval(iniciarTemporizador, 10);
     }
 
-    /*Button to Stop Timer*/
-    buttonStop.onclick = function(){
+    /*Boton para detener*/
+    botonDetener.onclick = function(){
         clearInterval(Interval);
     }
 
-    /*Button to Resst Timer*/
-    buttonReset.onclick = function(){
+    /*Boton para reiniciar*/
+    botonReiniciar.onclick = function(){
         clearInterval(Interval);
-        tens = "00";
-        seconds = "00";
-        appendTens.innerHTML = tens;
-        appendSeconds.innerHTML = seconds;
+        decenas = "00";
+        segundos = "00";
+        appendTens.innerHTML = decenas;
+        appendSeconds.innerHTML = segundos;
     }
 
+           
 }
