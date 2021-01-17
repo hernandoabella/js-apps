a08a02d75bb740adfb3a267ebcae2b2bef6db102
Hernando <hernandoabella@gmail.com>
2021-01-17 14:03:22 -0500

    adding new features

--- 2. Convertir de Horas a Segundos/script.js
+++ 2. Convertir de Horas a Segundos/script.js
@@ -4,10 +4,12 @@ let minutos = 60;
 let btn = document.getElementById('btn');
 let resultado = document.getElementById('resultado'); 
 
+
+//Esta función convierte las horas en segundos
 function convertir(hora){
    let formula = hora.value * minutos * 60;
    resultado.innerHTML = formula + ' segundos. ';
-   resultado.style.display = 'block';
+   resultado.style.visibility = 'visible';
    return resultado;
 }
 
