40e8a9a2cbb0f2b77b53aaf7e56e5311bf0ead38
Hernando <hernandoabella@gmail.com>
2021-01-16 23:54:22 -0500

    new chnages

--- 1. Hola Mundo/script.js
+++ 1. Hola Mundo/script.js
@@ -3,14 +3,7 @@ let saludo;
 saludo = document.getElementById('saludo');
 
 saludo.innerHTML = '<h1>Hola Mundo</h1>'; 
-saludo.style.position = 'absolute';
-saludo.style.top = '50%';
-saludo.style.left = '40%';
-saludo.style.color = 'red';
-saludo.style.fontSize = '20px';
 saludo.style.fontFamily = 'Montserrat, sans-serif';
-saludo.style.textAlign = 'center';
-
 
 
 
