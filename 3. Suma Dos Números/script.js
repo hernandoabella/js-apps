1f1cfdab734cd88843dbd9e91abf4274478775f6
Hernando <hernandoabella@gmail.com>
2021-01-18 02:18:07 -0500

    staff

--- 3. Suma Dos Números/script.js
+++ 3. Suma Dos Números/script.js
@@ -4,6 +4,8 @@ let a = document.getElementById('a');
 let b = document.getElementById('b');
 let resultado = document.getElementById('resultado');
 
+
+//Esta función te permite sumar dos números
 function sumar(a, b){
    let suma = +a.value + +b.value;
    resultado.style.display = 'block';
