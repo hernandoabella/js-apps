26f1a249cbc449a59cc6e4d8701dbf072d3a6102
Hernando <hernandoabella@gmail.com>
2021-01-18 22:35:18 -0500

    new animation added

--- 4. Contador/script.js
+++ 4. Contador/script.js
@@ -1,15 +1,23 @@
 //variables
 
-let numero = document.getElementById('numero');
+const numero = document.getElementById('numero');
 let btn1 = document.getElementById('btn1');
 let btn2 = document.getElementById('btn2');
 
 const sumar = () => {
+   numero.classList.remove('claseAnimacion');
+   numero.classList.add('claseAnimacion');
    return numero.innerText++;
 }
 
-
 const restar = () => {
+
+      numero.classList.remove('claseAnimacion');
+
+   window.requestAnimationFrame(() =>{
+
+      numero.classList.add('claseAnimacion');
+   })
    return numero.innerText--;
 }
 
