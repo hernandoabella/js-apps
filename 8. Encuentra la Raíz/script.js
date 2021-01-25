82bf13fe600679ee92ebd987bd213070a0ea2622
Hernando <hernandoabella@gmail.com>
2021-01-25 15:16:32 -0500

    nuevos cambios

--- 8. Encuentra la Raíz/script.js
+++ 8. Encuentra la Raíz/script.js
@@ -4,12 +4,12 @@ let entrada = document.querySelector('input');
 
 function raizCuadrada() {
     resultado.style.display = 'block';
-
+    
     let valorEntrada = entrada.value;
     
     let cadenaDeTexto = 'La raíz de ' + valorEntrada + 
-        ' es: ' + Math.sqrt(valorEntrada) + ' porque ' + 
-        ' ' + Math.sqrt(valorEntrada) + ' * ' + 
+        ' es: ' + Math.sqrt(valorEntrada) + ' ya que ' + 
+        ' ' + Math.sqrt(valorEntrada) + ' multiplicado por ' + 
         Math.sqrt(valorEntrada) + ' es igual a ' + valorEntrada; 
     
     return resultado.innerText = cadenaDeTexto;
