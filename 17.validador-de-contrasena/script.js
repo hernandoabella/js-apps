9b58c8d00bac79c7d7dfebaf32cae0ab129e0e02
Hernando <hernandoabella@gmail.com>
2021-02-11 11:00:30 -0500

    almost completed

--- 17.validador-de-contrasena/script.js
+++ 17.validador-de-contrasena/script.js
@@ -1,68 +1,56 @@
 var is_visible = false ;
 
-function see()
-{
+function see(){
     var input = document.getElementById("password");
     var see = document.getElementById("see");
     
-    if(is_visible)
-    {
+    if(is_visible){
         input.type = 'password';
         is_visible = false; 
         see.style.color='gray';
-    }
-    else
-    {
+    }else{
         input.type = 'text';
         is_visible = true; 
         see.style.color='#262626';
     }
-    
 }
 
-function check()
-{
+function validar(){
+
     var input = document.getElementById("password").value;
     
     input=input.trim();
     document.getElementById("password").value=input;
 
     document.getElementById("count").innerText="Length : " + input.length;
-    if(input.length>=5)
-    {
-        document.getElementById("check0").style.color="green";
-    }
-    else
-    {
-       document.getElementById("check0").style.color="tomato"; 
-    }
     
-    if(input.length<=10)
-    {
-        document.getElementById("check1").style.color="green";
-    }
-    else
-    {
-       document.getElementById("check1").style.color="tomato"; 
-    }
-    
-    if(input.match(/[0-9]/i))
-    {
-       document.getElementById("check2").style.color="green";
+    if(input.length>=5){
+        document.getElementById("validar0").style.color="green";
     }else{
-       document.getElementById("check2").style.color="tomato"; 
+       document.getElementById("validar0").style.color="red"; 
+    }
+    
+    if(input.length<=10){
+        document.getElementById("validar1").style.color="green";
+    }else{
+       document.getElementById("validar1").style.color="red"; 
+    }
+    
+    if(input.match(/[0-9]/i)){
+       document.getElementById("validar2").style.color="green";
+    }else{
+       document.getElementById("validar2").style.color="red"; 
     }
     
     if(input.match(/[^A-Za-z0-9-' ']/i)){
-        document.getElementById("check3").style.color="green";
+        document.getElementById("validar3").style.color="green";
     }else{
-        document.getElementById("check3").style.color="tomato"; 
+        document.getElementById("validar3").style.color="red"; 
     }
     
     if(input.match(' ')){
-        document.getElementById("check4").style.color="tomato";
+        document.getElementById("validar4").style.color="red";
     }else{
-        document.getElementById("check4").style.color="green"; 
+        document.getElementById("validar4").style.color="green"; 
     }
-    
 }
