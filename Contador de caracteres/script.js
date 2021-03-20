5f40953125b6169c94abb992f61a29153d0cbecc
Hernando Abella <hernandoabella@gmail.com>
2021-03-20 17:07:01 -0500

    adding some better styles

--- Contador de caracteres/script.js
+++ Contador de caracteres/script.js
@@ -1,8 +1,9 @@
-var print = function (msg)
-{
+let print = (msg) => {
     document.getElementById("output").innerHTML = "Length is " + msg + " (White Spaces are included)";
 }
-document.getElementById("btn").onclick = function (event)
-{
+
+
+
+document.getElementById("btn").onclick = function (event){
     print(document.getElementById('str').value.length);
 }
