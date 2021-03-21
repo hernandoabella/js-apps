a5601538fca1c4872cb1ed7f182d2f5e85e0bf71
Hernando Abella <hernandoabella@gmail.com>
2021-03-20 20:49:47 -0500

    program completed!

--- Contador de caracteres/script.js
+++ Contador de caracteres/script.js
@@ -1,9 +1,20 @@
+let boton = document.getElementById("boton");
+let cadena = document.getElementById("cadena");
+
 let imprimir = (mensaje) => {
-    document.getElementById("salida").innerHTML = "Length is " + mensaje + " (White Spaces are included)";
+    let salida = document.getElementById("salida");
+    salida.style.display = "block";
+    salida.innerHTML = "El largo de caracteres es: " + "<b>" + mensaje + "</b>" + " (Incluyendo los espacios en blanco)";
 }
 
+cadena.addEventListener("keyup", (e) =>{
+    if(e.key === 'Enter'){
+        e.preventDefault();
+        boton.click();
+    }
+}, false);
 
-
-document.getElementById("boton").onclick = function (event){
+boton.onclick = (e) =>{
     imprimir(document.getElementById('cadena').value.length);
 }
+
