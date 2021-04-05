4038f34300afc1c9da680a7f5136d22d08e44d7b
Hernando Abella <hernandoabella@gmail.com>
2021-04-05 10:15:10 -0500

    e

--- 3.tu_edad_en_dias/script.js
+++ 3.tu_edad_en_dias/script.js
@@ -1,15 +1,17 @@
-// Function to calculate number of days since you were born.
+// Función para calcular el número de días desde que naciste.
 function ageInDays() {
-    var birthYear = prompt("What year were you born... Good friend?");
-    var ageInDayss = (2019 - birthYear) * 365;
-    var h1 = document.createElement("h1");
-    var textAnswer = document.createTextNode("You are " + ageInDayss + " days old.");
+    let entrada = document.getElementById('entrada').value;
+    let resultado = document.getElementById("resultado");
+    let ageInDayss = (2019 - entrada) * 365;
+    let h1 = document.createElement("h1");
+    let textAnswer = document.createTextNode("Tienes " + ageInDayss + " días de edad.");
     h1.setAttribute("id", "ageInDays");
     h1.appendChild(textAnswer);
-    document.getElementById("flex-box-result").appendChild(h1);
+    resultado.appendChild(h1);
+    resultado.style.display = "block";
 }
 
-// Function to reset the page.
+// Función de reinicio
 function reset() {
     document.getElementById("ageInDays").remove();
 }
