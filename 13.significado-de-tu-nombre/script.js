638634ad98e22145a8860e7adab7874f57263e2f
Hernando Abella <hernandoabella@gmail.com>
2021-05-02 01:02:40 -0500

    e

--- 13.significado-de-tu-nombre/script.js
+++ 13.significado-de-tu-nombre/script.js
@@ -27,88 +27,88 @@ for (i = 0; i < len_; i++){
 name3 = name2.charAt(i);
 
 switch(name3) {
-case "a":
-name4 = "Amazing"
-break;
-case "b":
-name4 = "Brilliant"
-break;
-case "c":
-name4 = "Cute"
-break;
-case "d":
-name4 = "Dazzling"
-break;
-case "e":
-name4 = "Elegant"
-break;
-case "f":
-name4 = "Fabulous"
-break;
-case "g":
-name4 = "Gorgeous"
-break;
-case "h":
-name4 = "Healthy"
-break;
-case "i":
-name4 = "Impressive"
-break;
-case "j":
-name4 = "Joyful"
-break;
-case "k":
-name4 = "Kind"
-break;
-case "l":
-name4 = "Lucky"
-break;
-case "m":
-name4 = "Marvelous"
-break; 
-case "n": 
-name4 = "Nice" 
-break; 
-case "o": 
-name4 = "Open-Minded" 
-break; 
-case "p": 
-name4 = "Pleasant" 
-break; 
-case "q": 
-name4 = "Quality Person" 
-break; 
-case "r": 
-name4 = "Respectful" 
-break; 
-case "s": 
-name4 = "Splendid" 
-break; 
-case "t": 
-name4 = "Trustworthy" 
-break; 
-case "u": 
-name4 = "Uncompromising " 
-break; 
-case "v": 
-name4 = "Valuable" 
-break; 
-case "w": 
-name4 = "Wonderful" 
-break; 
-case "x": 
-name4 = "Xenial" 
-break; 
-case "y": 
-name4 = "Youthful" 
-break; 
-case "z": 
-name4 = "Zealous" 
-break; 
-default:
-name3 = "";
-name1 = "";
-name4 = ""
+    case "a":
+    name4 = "Amable"
+    break;
+    case "b":
+    name4 = "Bueno"
+    break;
+    case "c":
+    name4 = "Cute"
+    break;
+    case "d":
+    name4 = "Dazzling"
+    break;
+    case "e":
+    name4 = "Elegante"
+    break;
+    case "f":
+    name4 = "Fabulous"
+    break;
+    case "g":
+    name4 = "Gorgeous"
+    break;
+    case "h":
+    name4 = "Healthy"
+    break;
+    case "i":
+    name4 = "Impressive"
+    break;
+    case "j":
+    name4 = "Joyful"
+    break;
+    case "k":
+    name4 = "Kind"
+    break;
+    case "l":
+    name4 = "Lucky"
+    break;
+    case "m":
+    name4 = "Marvelous"
+    break; 
+    case "n": 
+    name4 = "Nice" 
+    break; 
+    case "o": 
+    name4 = "Open-Minded" 
+    break; 
+    case "p": 
+    name4 = "Pleasant" 
+    break; 
+    case "q": 
+    name4 = "Quality Person" 
+    break; 
+    case "r": 
+    name4 = "Respectful" 
+    break; 
+    case "s": 
+    name4 = "Splendid" 
+    break; 
+    case "t": 
+    name4 = "Trustworthy" 
+    break; 
+    case "u": 
+    name4 = "Uncompromising " 
+    break; 
+    case "v": 
+    name4 = "Valuable" 
+    break; 
+    case "w": 
+    name4 = "Wonderful" 
+    break; 
+    case "x": 
+    name4 = "Xenial" 
+    break; 
+    case "y": 
+    name4 = "Youthful" 
+    break; 
+    case "z": 
+    name4 = "Zafiro" 
+    break; 
+    default:
+    name3 = "";
+    name1 = "";
+    name4 = ""
 }
 
 document.getElementById("div22").innerHTML += "<span class='name3'>"+name3+"</span><span class='name4'> - "+name4+"</span><br>";
