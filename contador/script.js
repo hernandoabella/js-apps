17376e9a7a4e7c5e8d86c7dfb2a59e0443bac556
Hernando Abella <hernandoabella@gmail.com>
2021-08-15 10:27:57 -0500

    some change

--- contador/script.js
+++ contador/script.js
@@ -1,8 +1,10 @@
 //Variables
 
 const numero = document.getElementById('numero');
+const numtoInt = parseInt(numero.innerHTML);
 let btn1 = document.getElementById('btn1');
 let btn2 = document.getElementById('btn2');
+
 
 const sumar = () => {
 
@@ -11,30 +13,54 @@ const sumar = () => {
    setTimeout(() =>{
       numero.classList.add('animation');
    }, 10);
-   
-   return numero.innerText++;
+
+   numero.innerText++;
+
+   if(numero.innerText === '0'){
+      numero.style.color = '#999';
+   }else if(numero.innerText < '0'){
+      numero.style.color = "red";
+   }else{
+      numero.style.color = "green";
+   }
+
+   console.log(numero.innerHTML);
+
+   return numero.innerText;
+
 }
 
 const restar = () => {
-   
+
    removeAnimationClass();
 
    setTimeout(() =>{
       numero.classList.add('animation2');
    }, 10);
-   
-   return numero.innerText--;
+
+   numero.innerText--;
+
+   if(numero.innerText === '0'){
+      numero.style.color = '#999';
+   }else if(numero.innerText < '0'){
+      numero.style.color = "red";
+   }else{
+      numero.style.color = "green";
+   }
+
+   console.log(numero.innerHTML);
+
+   return numero.innerText;
 }
 
 const removeAnimationClass = () => {
    numero.classList.remove('animation2', 'animation');
 }
 
-
 // Activar contador automático: 
 
 // Suma automáticamente
-// setInterval(sumar, 1000); 
-
+// setInterval(sumar, 1000);
+ 
 // Resta automáticamente
 // setInterval(restar, 1000); 
