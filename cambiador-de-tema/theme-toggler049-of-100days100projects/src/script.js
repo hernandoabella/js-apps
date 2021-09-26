e7151bfd222d5cb20958875268e3c44ff33c37cb
Hernando Abella <hernandoabella@gmail.com>
2021-09-25 22:36:46 -0500

    nuevos programas agregados

---
+++ cambiador-de-tema/theme-toggler049-of-100days100projects/src/script.js
@@ -0,0 +1,18 @@
+const chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+});
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
