1e4d4fffd351b23f3644e63497d241e47327fafb
Hernando Abella <hernandoabella@gmail.com>
2021-11-10 03:05:55 -0500

    adding new program

--- contador-de-ano-nuevo/script.js
+++ contador-de-ano-nuevo/script.js
@@ -10,10 +10,10 @@ const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
-// Set background year
+// Ajustar año de fondo
 year.innerText = currentYear + 1;
 
-// Update countdown time
+// Actualizar tiempo del contador
 function updateCountdown() {
   const currentTime = new Date();
   const diff = newYearTime - currentTime;
@@ -23,18 +23,18 @@ function updateCountdown() {
   const m = Math.floor(diff / 1000 / 60) % 60;
   const s = Math.floor(diff / 1000) % 60;
 
-  // Add values to DOM
+  // Agregar valores al DOM
   days.innerHTML = d;
   hours.innerHTML = h < 10 ? '0' + h : h;
   minutes.innerHTML = m < 10 ? '0' + m : m;
   seconds.innerHTML = s < 10 ? '0' + s : s;
 }
 
-// Show spinner before countdown
+// Mostrar spinner antes del contador
 setTimeout(() => {
   loading.remove();
   countdown.style.display = 'flex';
 }, 1000);
 
-// Run every second
+// Correr cada 1 segundo
 setInterval(updateCountdown, 1000);
