b5b7e394a200f5d75d929775e98c9ad418f16268
Hernando Abella <hernandoabella@gmail.com>
2022-05-01 16:35:49 -0500

    updating

--- contador-de-seguidores/script.js
+++ contador-de-seguidores/script.js
@@ -3,7 +3,7 @@ const speed = 250;
 
 counters.forEach(item => {
     function updateCount() {
-        // + will convert the string (data-attribute and innerText(0)) to number
+        // El signo de + convertirá la cadena (data-attribute e innerText(0)) en un número 
         const target = +item.getAttribute('data-target');
         const count = +item.innerText;
 
@@ -11,12 +11,14 @@ counters.forEach(item => {
 
         if(count < target) {
             item.innerText = count + inc;
-            // run the updateCount every 1ms
+            // Ejecuta la función updateCount() cada 1ms
             setTimeout(updateCount, 1);
         }
         else {
             item.innerText = target;
         }
     }
+
     updateCount();
-})
+});
+
