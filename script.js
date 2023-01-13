f8f89711b2d79a05845d8f5e132bd60fba34cfdb
Hernando Abella <hernandoabella@gmail.com>
2023-01-12 22:39:15 -0500

    mejoras hermosas

--- script.js
+++ script.js
@@ -1,34 +1,12 @@
 // modo oscuro
-const modoOscuro = () => {
-    document.body.classList.toggle('dark-mode');
+// const modoOscuro = () => {
+//     document.body.classList.toggle('dark-mode');
+// }
+
+// dynamic copyright
+function copyright() {
+    let year = new Date().getFullYear();
+    let yearRef = document.querySelector('.year');
+    yearRef.innerHTML = year;
 }
-
-function myFunction() {
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
-
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
-
-function myFunction2() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
+copyright();
