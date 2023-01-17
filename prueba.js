a3055d93518d3c1556cae54008c1ff97ff2c760c
Hernando Abella <hernandoabella@gmail.com>
2023-01-16 21:42:57 -0500

    mejoras en la ui

---
+++ prueba.js
@@ -0,0 +1,19 @@
+// Programas de patrones #1
+
+// let cadenaDeTexto = "";
+
+// for(let i = 1; i <= 5; i++) {
+//     for(let j = 1; j <= 5; j++) {
+//         cadenaDeTexto += "*";
+//     }
+//     cadenaDeTexto += "\n";
+// }
+// console.log(cadenaDeTexto);
+
+// Resultado:
+
+// *****
+// *****
+// *****
+// *****
+// *****
