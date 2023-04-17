d04f28e64310229f536a31f3a6b5f43263e2349a
Hernando Abella <hernandoabella@gmail.com>
2023-04-16 23:06:40 -0500

    ajustando la seccion featured

--- src/components/Featured/Featured.tsx
+++ src/components/Featured/Featured.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import styles from './Featured.module.css';
+import featuredImg from '/public/featured-image.png';
 
 const Featured = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <img src="/path/to/image.jpg" alt="Featured" />
-      <h2 className="text-3xl font-bold my-4">Featured Title</h2>
-      <p className="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded mt-4">Call to Action</button>
+    <div className={styles.featured}>
+      <div className={styles.appContainer}>
+        <div className={styles.app}>
+          <image src={featuredImg} alt="Featured App" className={styles.appImage} />
+          <h2 className={styles.appTitle}>Featured App Title</h2>
+          <p className={styles.appDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in rutrum purus. Sed non enim tincidunt, varius orci non, fringilla lorem.</p>
+          <button className={styles.ctaButton}>Download now</button>
+        </div>
+        <div className={styles.otherApps}>
+          <div className={styles.otherApp}>
+            <image src="/public/featured-image.png" alt="Other App 1" className={styles.otherAppImage} />
+            <h3 className={styles.otherAppTitle}>Other App 1 Title</h3>
+          </div>
+          <div className={styles.otherApp}>
+            <image src="/path/to/other-image2.jpg" alt="Other App 2" className={styles.otherAppImage} />
+            <h3 className={styles.otherAppTitle}>Other App 2 Title</h3>
+          </div>
+          <div className={styles.otherApp}>
+            <image src="/path/to/other-image3.jpg" alt="Other App 3" className={styles.otherAppImage} />
+            <h3 className={styles.otherAppTitle}>Other App 3 Title</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
