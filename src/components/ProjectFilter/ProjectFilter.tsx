50fee877c2766ed2fea049f7868de301f3d826bb
Hernando Abella <hernandoabella@gmail.com>
2023-04-22 21:12:33 -0500

    mejora de ancho de las tarjetas...

--- src/components/ProjectFilter/ProjectFilter.tsx
+++ src/components/ProjectFilter/ProjectFilter.tsx
@@ -1,31 +1,47 @@
 import React from "react";
+import { FaStar } from "react-icons/fa";
 
 const ProjectFilter = () => {
   return (
-    <div className="flex justify-between py-4 px-8 bg-gray-100">
-      <div className="flex flex-col mr-4">
+    <div className="flex justify-center items-center py-10">
+      <div className="flex items-center mr-4">
+        <label htmlFor="nameFilter" className="mr-2">
+          Filtrar por nombre:
+        </label>
         <input
+          id="nameFilter"
           type="text"
-          placeholder="Filtrar por nombre"
-          className="rounded-lg border border-gray-400 p-2 mb-2 focus:outline-none focus:ring-2 focus:ring-gray-400"
+          placeholder="Escribe aquí..."
+          className="rounded-lg border border-gray-400 p-2 focus:outline-none focus:ring-2 focus:ring-gray-400"
         />
-        <select className="rounded-lg border border-gray-400 p-2 focus:outline-none focus:ring-2 focus:ring-gray-400">
-          <option value={0}>Filtrar por dificultad</option>
-          <option value={1}>1 estrella</option>
-          <option value={2}>2 estrellas</option>
-          <option value={3}>3 estrellas</option>
-          <option value={4}>4 estrellas</option>
-          <option value={5}>5 estrellas</option>
+      </div>
+      <div className="flex items-center mr-4">
+        <label htmlFor="ratingFilter" className="mr-2">
+          Filtrar por dificultad:
+        </label>
+        <select
+          id="ratingFilter"
+          className="rounded-lg border border-gray-400 p-2 focus:outline-none focus:ring-2 focus:ring-gray-400"
+        >
+          <option value="<FaStar />"> 1 estrella</option>
+          <option value={1}><FaStar  /> 2 estrellas</option>
+          <option value={2}><FaStar  /> 3 estrellas</option>
+          <option value={3}><FaStar  /> 4 estrellas</option>
+          <option value={4}><FaStar  /> 5 estrellas</option>
         </select>
       </div>
-      <div className="flex flex-col">
-        <label className="text-gray-600 font-medium mb-2">Categoría:</label>
-        <select className="rounded-lg border border-gray-400 p-2 focus:outline-none focus:ring-2 focus:ring-gray-400">
+      <div className="flex items-center">
+        <label htmlFor="categoryFilter" className="mr-2">
+          Filtrar por categoría:
+        </label>
+        <select
+          id="categoryFilter"
+          className="rounded-lg border border-gray-400 p-2 focus:outline-none focus:ring-2 focus:ring-gray-400"
+        >
           <option value={0}>Todas las categorías</option>
-          <option value={1}>Diseño gráfico</option>
-          <option value={2}>Desarrollo web</option>
-          <option value={3}>Marketing</option>
-          <option value={4}>Redes sociales</option>
+          <option value={1}>Texto</option>
+          <option value={2}>Imágenes</option>
+          <option value={3}>Color</option>
         </select>
       </div>
     </div>
