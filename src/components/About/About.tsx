e7c84875a6ff1ff4ed613530add327f3b36f94ff
Hernando Abella <hernandoabella@gmail.com>
2023-04-28 23:15:26 -0500

    mejoras en about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -9,27 +9,21 @@ const About = () => {
         <h3 className="mt-2 text-2xl font-bold leading-8 tracking-tight text-gray-900 sm:text-3xl">Desafía tu habilidad como desarrollador de JavaScript</h3>
         <p className="mt-4 text-base text-gray-500">JS-APPS proporciona proyectos de JavaScript para que puedas desafiarte a ti mismo y convertirte en un mejor desarrollador.</p>
       </div>
-    
-      <div className="flex flex-col items-center justify-between mt-8 md:flex-row md:px-16">
+      <div className="flex flex-col items-center justify-between mt-20 md:flex-row">
         <div className="mb-4 sm:mb-0 sm:mr-4 flex flex-col items-center">
-          <Image src="/icon1.png" alt="Icono 1" width={50} height={50} className="mb-2" />
-          <p className="text-sm text-gray-500 ">Aprender haciendo. Construye muchos proyectos pequeños y medianos para mejorar tus habilidades.</p>
+          <Image src="/icon1.png" alt="Icono 1" width={100} height={100} className="mb-5" />
+          <p className="text-gray-500">Aprender haciendo. Construye muchos proyectos pequeños y medianos para mejorar tus habilidades.</p>
         </div>
-    
         <div className="mb-4 sm:mb-0 sm:mx-4 flex flex-col items-center">
-          <Image src="/icon2.png" alt="Icono 2" width={50} height={50} className="mb-2" />
-          <p className="text-sm text-gray-500 text-center">Selecciona uno de los proyectos sugeridos aquí para aprender algo nuevo o reutilizar técnicas ya aprendidas.</p>
+          <Image src="/icon2.png" alt="Icono 2" width={100} height={100} className="mb-5" />
+          <p className="text-gray-500">Selecciona uno de los proyectos sugeridos aquí para aprender algo nuevo o reutilizar técnicas ya aprendidas.</p>
         </div>
-    
         <div className="flex flex-col items-center">
-          <Image src="/icon3.png" alt="Icono 3" width={50} height={50} className="mb-2" />
-          <p className="text-sm text-gray-500 text-center">JS-APPS desafía a los desarrolladores a practicar la sintaxis de JavaScript mediante la creación de proyectos.</p>
+          <Image src="/icon3.png" alt="Icono 3" width={100} height={100} className="mb-5" />
+          <p className="text-gray-500">JS-APPS desafía a los desarrolladores a practicar la sintaxis de JavaScript mediante la creación de proyectos.</p>
         </div>
       </div>
     </div>
-    
-    
-    
   );
 };
 
