82e5ff0ec0132d5c75ad68952d522e3732251f94
Hernando Abella <hernandoabella@gmail.com>
2023-05-03 16:45:06 -0500

    testimonials dinamico

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -1,41 +1,34 @@
 import React from 'react';
 import Image from 'next/image';
+import testimonialsData from '../../../data/testimonials.json';
+
+type Testimonial = {
+  id: number;
+  text: string;
+  name: string;
+  jobTitle: string;
+  photo: string;
+};
 
 const Testimonials = () => {
+  const testimonials: Testimonial[] = testimonialsData;
+
   return (
     <div className="bg-white py-12 text-center">
       <h2 className="text-4xl font-bold text-gray-800 mb-8">Testimonios de nuestros clientes</h2>
-      <div className="flex justify-center flex-wrap ">
-        <div className="bg-white rounded-lg shadow-md mx-4 my-8 p-8 w-80">
-          <p className="text-gray-600 text-lg leading-7 mb-6">Me encantó la aplicación. Me ayudó a ser más productivo y organizado en mi día a día.</p>
-          <div className="flex items-center">
-            <Image src="/customer1.jpg" alt="Customer 1" width={60} height={60} className="rounded-full mr-3" />
-            <div>
-              <p className="font-medium text-gray-800 text-lg mb-1">Juan Pérez</p>
-              <p className="text-gray-500 text-sm">CEO en ABC Company</p>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        {testimonials.map((testimonial) => (
+          <article key={testimonial.id} className="bg-white rounded-lg shadow-md flex flex-col justify-between p-8">
+            <p className="text-gray-600 text-lg leading-7 mb-6 flex-1">{testimonial.text}</p>
+            <div className="flex items-center">
+              <Image src={testimonial.photo} alt={testimonial.name} width={60} height={60} className="rounded-full mr-3" />
+              <div>
+                <p className="font-medium text-gray-800 text-lg mb-1">{testimonial.name}</p>
+                <p className="text-gray-500 text-sm">{testimonial.jobTitle}</p>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="bg-white rounded-lg shadow-md mx-4 my-8 p-8 w-80">
-          <p className="text-gray-600 text-lg leading-7 mb-6"> La mejor aplicación que he utilizado. Me permite trabajar en cualquier lugar y momento, sin preocuparme por la seguridad de mis datos.</p>
-          <div className="flex items-center">
-            <Image src="/customer2.jpg" alt="Customer 2" width={60} height={60} className="rounded-full mr-3" />
-            <div>
-              <p className="font-medium text-gray-800 text-lg mb-1">María González</p>
-              <p className="text-gray-500 text-sm">Diseñadora gráfica freelance</p>
-            </div>
-          </div>
-        </div>
-        <div className="bg-white rounded-lg shadow-md mx-4 my-8 p-8 w-80">
-          <p className="text-gray-600 text-lg leading-7 mb-6">Esta aplicación ha cambiado mi vida. Me ayuda a mantenerme enfocado en mis tareas y proyectos, y me permite alcanzar mis metas con mayor facilidad.</p>
-          <div className="flex items-center">
-            <Image src="/customer3.webp" alt="Customer 3" width={60} height={60} className="rounded-full mr-3" />
-            <div>
-              <p className="font-medium text-gray-800 text-lg mb-1">Ana Ramírez</p>
-              <p className="text-gray-500 text-sm">Profesora de yoga</p>
-            </div>
-          </div>
-        </div>
+          </article>
+        ))}
       </div>
     </div>
   );
