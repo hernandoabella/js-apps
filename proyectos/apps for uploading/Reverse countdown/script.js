b2930a51b197fa775b41bce417739186f7873ccf
Hernando Abella <hernandoabella@gmail.com>
2023-06-11 20:23:36 -0500

    proyectos folder removed

--- proyectos/apps for uploading/Reverse countdown/script.js
+++
@@ -1,8 +0,0 @@
-var number = document.querySelector(".number");  
-var count = 20;  
-// Countdown Interval which runs on every 1s  
-var countdownInterval = setInterval(() => {  
- // if count is less than or equal to 1 then clear the Interval  
- count <= 1 && clearInterval(countdownInterval);  
- number.textContent = count <= 10 ? `0${--count}` : `${--count}`;  
-}, 1000);  
--- proyectos/apps for uploading/juego-de-velocidad-de-tipado/JS_SpeedTypingGame/README.md
+++
@@ -1,2 +0,0 @@
-# JS_SpeedTypingGame
-This project is a game created with HTML, CSS and Javascript using Random Quotes Api
