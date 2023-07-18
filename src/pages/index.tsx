916ff258b6683e27f48fc829d0f50e774f1ac6a0
Hernando Abella <hernandoabella@gmail.com>
2023-07-18 02:09:22 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
 // import Image from "next/image";
 // import { Inter } from "next/font/google";
-// import Header from "../components/Header/Header";
+import Header from "../components/Header/Header";
 // import Hero from "../components/Hero/Hero";
 // import About from "../components/About/About";
 // import Featured from "../components/Featured/Featured";
 // import Testimonials from "../components/Testimonials/Testimonials";
 // import Faq from "../components/Faq/Faq";
 // import Cta from "../components/Cta/Cta";
+import Projects from "../components/Projects/Projects";
 import Footer from "../components/Footer/Footer";
 
 export default function Home() {
   return (
     <div>
-      {/* <Header />
+      {/* 
     <Hero />
     <About />
     <Featured />
@@ -20,7 +21,8 @@ export default function Home() {
     <Faq />
     <Cta />
     <Footer /> */}
-      
+      <Header />
+      <Projects />
       <Footer />
     </div>
   );
