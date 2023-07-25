6b874959857cdf199479bf58246a9d8d8a44356d
Hernando Abella <hernandoabella@gmail.com>
2023-07-24 19:42:32 -0500

    Update ProjectCard.tsx

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -62,7 +62,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
 
   return (
     <div className="rounded-xl overflow-hidden shadow-xl">
-      <div className="w-full h-72 md:h-72 lg:h-80">
+      <div className="w-full h-96 md:h-96 lg:h-96">
         <iframe src={demoLink} className="w-full h-full" title={name}></iframe>
       </div>
 
@@ -71,54 +71,11 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
         <p className="text-justify mb-6 text-slate-600 dark:text-slate-400">
           {description}
         </p>
-        <div>
-          <div className="flex items-center mb-4">
-            <p className="font-bold mr-2">Nivel:</p>
-            <div className="flex">
-              {[...Array(difficulty)].map((_, index) => (
-                <FaStar key={index} className="text-yellow-400" />
-              ))}
-            </div>
-          </div>
-          <div className="flex items-center mb-4">
-            <p className="font-bold mr-2">Categoría:</p>
-            <p className="text-slate-600 dark:text-slate-400">{category}</p>
-          </div>
-          <div className="flex items-center pb-4">
-            <p className="font-bold mr-2">Tecnologías:</p>
-
-            <div className="flex items-center align-middle">
-              <div className="flex items-center mr-2">
-                <Image
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
-                  alt="HTML5"
-                  width={20}
-                  height={20}
-                />
-              </div>
-              <div className="flex items-center mr-2">
-                <Image
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
-                  alt="CSS3"
-                  width={20}
-                  height={20}
-                />
-              </div>
-              <div className="flex items-center">
-                <Image
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
-                  alt="JavaScript"
-                  width={20}
-                  height={20}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex space-x-4 w-full text-center">
+        
+        <div className="flex w-full text-center">
           <a
             href={downloadLink}
-            className="md:w-auto bg-blue-600 hover:bg-blue-700 text-white rounded p-4 flex items-center justify-center transition duration-300"
+            className="mr-5 flex-1 bg-blue-600 hover:bg-blue-700 text-white rounded-lg p-4 flex items-center justify-center transition duration-300"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -126,7 +83,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
           </a>
           <a
             href={demoLink}
-            className="md:w-auto bg-green-600 hover:bg-green-700 text-white rounded p-4 flex items-center justify-center transition duration-300"
+            className="flex-1 bg-green-600 hover:bg-green-700 text-white rounded-lg p-4 flex items-center justify-center transition duration-300"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -134,53 +91,6 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
           </a>
         </div>
       </div>
-
-      {showCode && (
-        <div className="bg-gray-200 dark:bg-slate-800 p-8 absolute">
-          <div className="mb-4">
-            <h3 className="text-lg font-bold mb-2">Código HTML</h3>
-            <div className="relative">
-              <SyntaxHighlighter language="html" style={vscDarkPlus}>
-                {htmlCode}
-              </SyntaxHighlighter>
-              <button
-                onClick={() => handleCopyCodeClick(0, htmlCode)}
-                className="absolute top-2 right-2 text-gray-400 hover:text-gray-600"
-              >
-                {copied[0] ? <FaCheck /> : <FaCopy />}
-              </button>
-            </div>
-          </div>
-          <div className="mb-4">
-            <h3 className="text-lg font-bold mb-2">Código CSS</h3>
-            <div className="relative">
-              <SyntaxHighlighter language="css" style={vscDarkPlus}>
-                {cssCode}
-              </SyntaxHighlighter>
-              <button
-                onClick={() => handleCopyCodeClick(1, cssCode)}
-                className="absolute top-2 right-2 text-gray-400 hover:text-gray-600"
-              >
-                {copied[1] ? <FaCheck /> : <FaCopy />}
-              </button>
-            </div>
-          </div>
-          <div>
-            <h3 className="text-lg font-bold mb-2">Código JavaScript</h3>
-            <div className="relative">
-              <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-                {jsCode}
-              </SyntaxHighlighter>
-              <button
-                onClick={() => handleCopyCodeClick(2, jsCode)}
-                className="absolute top-2 right-2 text-gray-400 hover:text-gray-600"
-              >
-                {copied[2] ? <FaCheck /> : <FaCopy />}
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
