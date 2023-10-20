dbdafe3b44b35c417b38b9e0108f50fe26c76035
Hernando Abella <hernandoabella@gmail.com>
2023-10-20 02:27:56 -0500

    Update ProjectCard.tsx

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,10 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  FaDownload,
-  FaStar,
-  FaExpand,
-  FaCopy,
-} from "react-icons/fa";
+import { FaDownload, FaStar, FaExpand, FaCopy, FaCode } from "react-icons/fa";
 import hljs from "highlight.js";
 import "highlight.js/styles/atom-one-dark.css";
 import CopyToClipboard from "react-copy-to-clipboard";
@@ -14,7 +9,7 @@ interface ProjectCardProps {
   description: string;
   difficulty: number;
   downloadLink: string;
-  githubLink: string; // Agrega githubLink a la interfaz
+  githubLink: string;
   demoLink: string;
   htmlCode: string;
   cssCode: string;
@@ -40,6 +35,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   javascriptCode,
 }) => {
   const [isFullScreen, setFullScreen] = useState(false);
+  const [showCode, setShowCode] = useState(true);
   const [copiedHTML, setCopiedHTML] = useState(false);
   const [copiedCSS, setCopiedCSS] = useState(false);
   const [copiedJavaScript, setCopiedJavaScript] = useState(false);
@@ -81,19 +77,20 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
     }
   };
 
-  // Function to handle copying HTML code
+  const toggleCode = () => {
+    setShowCode(!showCode);
+  };
+
   const handleCopyHTML = () => {
     setCopiedHTML(true);
     setTimeout(() => setCopiedHTML(false), 2000);
   };
 
-  // Function to handle copying CSS code
   const handleCopyCSS = () => {
     setCopiedCSS(true);
     setTimeout(() => setCopiedCSS(false), 2000);
   };
 
-  // Function to handle copying JavaScript code
   const handleCopyJavaScript = () => {
     setCopiedJavaScript(true);
     setTimeout(() => setCopiedJavaScript(false), 2000);
@@ -101,7 +98,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
 
   return (
     <div className="shadow-lg flex">
-      <div className="w-1/2 pr-4">
+      <div className={showCode ? "w-1/2" : "w-full"}>
         <div className="h-96">
           <div className="flex justify-between relative">
             <div className="bg-slate-300 text-slate-500 dark:bg-slate-800 text-center p-2 dark:text-yellow-400 gap-2 flex absolute">
@@ -123,81 +120,99 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
             title={name}
           ></iframe>
         </div>
-        <div className="p-8 flex flex-col justify-between bg-slate-200 dark:bg-slate-800">
+        <div className="p-8 flex flex-col justify-between bg-slate-200 dark-bg-slate-800">
           <h2 className="font-bold text-2xl mb-4 text-center">{name}</h2>
           <p className="text-justify mb-6 text-slate-400 dark-text-slate-400">
             {description}
           </p>
-          <div className="flex flex-col md:flex-row w-full text-center">
+          <div className="flex flex-col md:flex-row w-full text-center gap-4">
             <a
               href={downloadLink}
-              className="md-mr-5 flex-1 bg-blue-600 hover-bg-blue-700 text-white rounded-lg p-4 flex items-center justify-center transition duration-300"
+              className="md-mr-2 flex-1 bg-blue-600 hover:bg-blue-700 text-white rounded-lg p-4 flex items-center justify-center transition duration-300"
               target="_blank"
               rel="noopener noreferrer"
             >
               <FaDownload className="mr-2" /> Download
             </a>
+            <button
+              className={`${
+                showCode
+                  ? "bg-red-600 hover:bg-red-700"
+                  : "bg-green-600 hover:bg-green-700"
+              } flex-1 text-white rounded-lg p-4 flex items-center justify-center transition duration-300`}
+              onClick={toggleCode}
+            >
+              {showCode ? <FaCode className="mr-2" /> : null}
+              {showCode ? "Hide Source Code" : "Show Source Code"}
+            </button>
           </div>
         </div>
       </div>
 
-      <div className="w-1/2 pl-4">
-        <h3 className="text-lg py-4 flex align-center items-center gap-1">
-          <i className="devicon-html5-plain"></i> HTML
-          <CopyToClipboard text={htmlCode} onCopy={handleCopyHTML}>
-            <FaCopy className="ml-2 cursor-pointer" />
-          </CopyToClipboard>
-          {copiedHTML && <span className="ml-2 text-green-600">Copied!</span>}
-        </h3>
-        <div className="h-36 overflow-y-auto">
-          <pre>
-            <code
-              className="html"
-              dangerouslySetInnerHTML={{
-                __html: hljs.highlight("html", htmlCode).value,
-              }}
-            />
-          </pre>
-        </div>
-        <h3 className="text-lg py-4 flex align-center items-center gap-1">
-          <div className="flex gap-4 items-center">
-            <i className="devicon-css3-plain"></i> CSS
-            <CopyToClipboard text={cssCode} onCopy={handleCopyCSS}>
+      {showCode && (
+        <div className="w-1/2 pl-4">
+          <h3 className="text-lg py-4 flex align-center items-center gap-1">
+            <i className="devicon-html5-plain"></i> HTML
+            <CopyToClipboard text={htmlCode} onCopy={handleCopyHTML}>
               <FaCopy className="ml-2 cursor-pointer" />
             </CopyToClipboard>
-            {copiedCSS && <span className="ml-2 text-green-600">Copied!</span>}
+            {copiedHTML && <span className="ml-2 text-green-600">Copied!</span>}
+          </h3>
+          <div className="h-36 overflow-y-auto">
+            <pre>
+              <code
+                className="html"
+                dangerouslySetInnerHTML={{
+                  __html: hljs.highlight("html", htmlCode).value,
+                }}
+              />
+            </pre>
           </div>
-        </h3>
-        <div className="h-36 overflow-y-auto">
-          <pre>
-            <code
-              className="css"
-              dangerouslySetInnerHTML={{
-                __html: hljs.highlight("css", cssCode).value,
-              }}
-            />
-          </pre>
+          <h3 className="text-lg py-4 flex align-center items-center gap-1">
+            <div className="flex gap-4 items-center">
+              <i className="devicon-css3-plain"></i> CSS
+              <CopyToClipboard text={cssCode} onCopy={handleCopyCSS}>
+                <FaCopy className="ml-2 cursor-pointer" />
+              </CopyToClipboard>
+              {copiedCSS && (
+                <span className="ml-2 text-green-600">Copied!</span>
+              )}
+            </div>
+          </h3>
+          <div className="h-36 overflow-y-auto">
+            <pre>
+              <code
+                className="css"
+                dangerouslySetInnerHTML={{
+                  __html: hljs.highlight("css", cssCode).value,
+                }}
+              />
+            </pre>
+          </div>
+          <h3 className="text-lg py-4 flex align-center items-center gap-1">
+            <i className="devicon-javascript-plain"></i> JavaScript
+            <CopyToClipboard
+              text={javascriptCode}
+              onCopy={handleCopyJavaScript}
+            >
+              <FaCopy className="ml-2 cursor-pointer" />
+            </CopyToClipboard>
+            {copiedJavaScript && (
+              <span className="ml-2 text-green-600">Copied!</span>
+            )}
+          </h3>
+          <div className="h-36 overflow-y-auto bg-slate-800 text-slate-50">
+            <pre>
+              <code
+                className="javascript"
+                dangerouslySetInnerHTML={{
+                  __html: hljs.highlight("javascript", javascriptCode).value,
+                }}
+              />
+            </pre>
+          </div>
         </div>
-        <h3 className="text-lg py-4 flex align-center items-center gap-1">
-          <i className="devicon-javascript-plain"></i> JavaScript
-          <CopyToClipboard text={javascriptCode} onCopy={handleCopyJavaScript}>
-            <FaCopy className="ml-2 cursor-pointer" />
-          </CopyToClipboard>
-          {copiedJavaScript && (
-            <span className="ml-2 text-green-600">Copied!</span>
-          )}
-        </h3>
-        <div className="h-36 overflow-y-auto">
-          <pre>
-            <code
-              className="javascript"
-              dangerouslySetInnerHTML={{
-                __html: hljs.highlight("javascript", javascriptCode).value,
-              }}
-            />
-          </pre>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
