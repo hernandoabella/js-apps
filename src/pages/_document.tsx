e8205db55388122d626995e17fd3f87c6d1f5fd9
Hernando Abella <hernandoabella@gmail.com>
2023-10-19 22:11:50 -0500

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
