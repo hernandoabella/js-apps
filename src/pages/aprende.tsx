38c1902fca0697e7d8e9770b9136e2dede64a9eb
Hernando Abella <hernandoabella@gmail.com>
2023-10-30 22:26:03 -0500

    Create aprende.tsx

---
+++ src/pages/aprende.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header/Header";
+
+import Footer from "../components/Footer/Footer";
+
+const AprendePage = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default AprendePage;
