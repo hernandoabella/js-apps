c0f97ff1371615ba693bb44650b2bd6de6c18654
Hernando Abella <hernandoabella@gmail.com>
2023-11-01 21:03:45 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,24 @@
-// import Image from "next/image";
-// import { Inter } from "next/font/google";
+import Image from "next/image";
+import { Inter } from "next/font/google";
 import Header from "../components/Header/Header";
-// import Hero from "../components/Hero/Hero";
-// import About from "../components/About/About";
-// import Featured from "../components/Featured/Featured";
-// import Testimonials from "../components/Testimonials/Testimonials";
-// import Faq from "../components/Faq/Faq";
-// import Cta from "../components/Cta/Cta";
+import Hero from "../components/Hero/Hero";
+import About from "../components/About/About";
+import Featured from "../components/Featured/Featured";
+import Testimonials from "../components/Testimonials/Testimonials";
+import Faq from "../components/Faq/Faq";
+import Cta from "../components/Cta/Cta";
 import Projects from "../components/Projects/Projects";
 import Footer from "../components/Footer/Footer";
 
 export default function Home() {
   return (
     <div>
-      {/* 
-    <Hero />
-    <About />
-    <Featured />
-    <Testimonials />
-    <Faq />
-    <Cta />
-    <Footer /> */}
-      <Header />
-      <Projects />
+      <Hero />
+      <About />
+      <Featured />
+      <Testimonials />
+      <Faq />
+      <Cta />
       <Footer />
     </div>
   );
