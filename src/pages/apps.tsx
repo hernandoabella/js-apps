276ea04eab616ac0e078c06d4026beaf170645d2
Hernando Abella <hernandoabella@gmail.com>
2023-11-01 23:47:14 -0500

    Create apps.tsx

---
+++ src/pages/apps.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Projects from "../components/Projects/Projects";
+
+const AplicacionesPage = () => {
+  return (
+    <>
+      <Header />
+      <Projects />
+      <Footer />
+    </>
+  );
+};
+
+export default AplicacionesPage;
