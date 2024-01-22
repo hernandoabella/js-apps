614cedbc429ae3039a939c8d92a4f8e70b6749f5
Hernando Abella <hernandoabella@gmail.com>
2024-01-21 23:14:13 -0500

    Update apps.tsx

--- src/pages/apps.tsx
+++ src/pages/apps.tsx
@@ -5,6 +5,23 @@ import Projects from "../components/Projects/Projects";
 const AplicacionesPage = () => {
   return (
     <>
+      <div className="p-8 fixed w-full z-10 shadow-xl">
+        <div className="flex justify-items-center items-center center place-content-center
+">
+          <a
+            href="https://hernandoabella.gumroad.com/l/hoocc"
+            target="_blank"
+            className="float right"
+          >
+            <button className="button">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 24">
+                <path d="m18 0 8 12 10-8-4 20H4L0 4l10 8 8-12z"></path>
+              </svg>
+              Download All Apps
+            </button>
+          </a>
+        </div>
+      </div>
       <Header />
       <Projects />
       <Footer />
