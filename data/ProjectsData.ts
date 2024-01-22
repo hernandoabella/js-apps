fb653785bd25cb414a2e186b55ec851974784572
Hernando Abella <hernandoabella@gmail.com>
2024-01-21 23:14:08 -0500

    Update ProjectsData.ts

--- data/ProjectsData.ts
+++ data/ProjectsData.ts
@@ -13,4 +13,18 @@ export const projectsData = [
     jsCode: `// JS Code
     `,
   },
+  {
+    name: "Text Resizer",
+    description: "...",
+    difficulty: 1,
+    downloadLink: "https://www.github.com/",
+    githubLink: "https://github.com/hernandoabella/redimensionador-de-texto",
+    demoLink: "https://effulgent-pasca-40a1c9.netlify.app/",
+    htmlCode: `<!-- HTML Code -->
+    `,
+    cssCode: `/* CSS Code */
+    `,
+    jsCode: `// JS Code
+    `,
+  },
 ];
