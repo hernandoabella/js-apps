fe23db20bc7cf99059a987f940bdbfb4e838186f
Hernando Abella <hernandoabella@gmail.com>
2024-01-21 22:21:37 -0500

    Update About.tsx

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className="p-8 md:p-20 dark:bg-slate-900 dark:text-white">
       <div className="text-center lg:text-left">
         <h2 className="text-base text-center text-sky-600 dark:text-sky-400 font-semibold tracking-wide uppercase mb-5">
-          About JS-APPS
+          About us
         </h2>
         <h3 className="text-4xl font-bold mb-5 text-center text-slate-900 dark:text-white">
           Challenge your skills as a JavaScript developer
@@ -19,49 +19,46 @@ const About = () => {
       <div className="flex flex-col items-center justify-center mt-10 md:flex-row">
         <div className="flex flex-col items-center w-full p-4">
           <Image
-            src="/icon1.png"
-            alt="Icono 1"
-            width={80}
-            height={80}
+            src="/learn-by-doing.png"
+            alt="learn-by-doing"
+            width={150}
+            height={150}
             className="mb-5"
           />
-          <h4 className="text-2xl font-bold m-3 text-center">
-          Learn by Doing
-          </h4>
+          <h4 className="text-2xl font-bold m-3 text-center">Learn by Doing</h4>
           <p className="text-slate-600 dark:text-slate-400 text-center">
-          Build many small and medium projects to level up.
+            Build many small and medium projects to level up.
           </p>
         </div>
         <div className="flex flex-col items-center w-full p-4">
           <Image
-            src="/icon2.png"
+            src="/suggested-project.webp"
             alt="Icono 2"
-            width={80}
-            height={80}
+            width={150}
+            height={150}
             className="mb-5"
           />
           <h4 className="text-2xl font-bold m-3 text-center">
-            Proyectos Sugeridos
+            Suggested Projects
           </h4>
           <p className="text-slate-600 dark:text-slate-400 text-center">
-            Selecciona uno de los proyectos sugeridos aquí para aprender algo
-            nuevo o reutilizar técnicas ya aprendidas.
+            Select one of the suggested projects here to learn something new or
+            reuse techniques already learned.
           </p>
         </div>
         <div className=" flex flex-col items-center w-full p-4">
           <Image
-            src="/icon3.png"
+            src="/challenge.webp"
             alt="Icono 3"
-            width={80}
-            height={80}
+            width={150}
+            height={150}
             className="mb-5"
           />
           <h4 className="text-2xl font-bold m-3 text-center">
-            Desafía Tus Habilidades
+            Challenge Your Skills
           </h4>
           <p className="text-slate-600 dark:text-slate-400 text-center">
-            Te desafiamosz a practicar la sintaxis de JavaScript mediante la
-            creación de proyectos.
+            We challenge you to practice JavaScript syntax by project creation.
           </p>
         </div>
       </div>
