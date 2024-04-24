7c396a7fcac5194c9efe48b20c3093e6bf8878e2
Hernando Abella <hernandoabella@gmail.com>
2024-04-23 23:32:17 -0500

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -13,16 +13,6 @@ function Footer() {
           </Link>
           . Creativity in your hands.
         </p>
-        <div className="flex justify-center text-slate-400 hover:text-slate-500 dark:hover:text-slate-300">
-          <a
-            href="https://www.buymeacoffee.com/hernandoabella"
-            target="_blank"
-            rel="noopener"
-            title="Buy me a coffee"
-          >
-            <FaCoffee className="hover:text-gray-600 mx-2 h-6 w-6" />
-          </a>
-        </div>
       </div>
     </footer>
   );
