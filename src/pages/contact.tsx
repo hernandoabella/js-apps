d5d0de9e0d93df40edc42e0199296ce758085980
Hernando Abella <hernandoabella@gmail.com>
2024-04-25 01:14:37 -0500

    Create contact.tsx

---
+++ src/pages/contact.tsx
@@ -0,0 +1,12 @@
+const Contact: React.FC = () => {
+    return (
+      <>
+        <div>
+          <h1>Contact</h1>
+          
+        </div>
+      </>
+    );
+  };
+  
+  export default Contact;
