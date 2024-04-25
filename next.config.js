58b769aa27ac6df37de1f239b42e680138979d3f
Hernando Abella <hernandoabella@gmail.com>
2024-04-25 01:14:30 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,7 +11,44 @@ const nextConfig = {
             value: 'public, max-age=3600',
           },
         ],
-      }
+      },
+      {
+        source: '/login',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600',
+          },
+        ],
+      },
+      {
+        source: '/contact',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600',
+          },
+        ],
+      },
+      {
+        source: '/why-us',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600',
+          },
+        ],
+      },
+      {
+        source: '/pricing',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600',
+          },
+        ],
+      },
+      // Add more entries for other pages as needed
     ];
   },
 };
