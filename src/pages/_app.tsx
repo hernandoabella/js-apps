edc1089256f77966d6ecd5b52066c4172355b81b
Hernando Abella <hernandoabella@gmail.com>
2024-04-25 23:34:22 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
-import '@/styles/globals.css';
-import { ThemeProvider } from 'next-themes';
-import type { AppProps } from 'next/app';
+import "@/styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import type { AppProps } from "next/app";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <ThemeProvider attribute="class">
-        <Component {...pageProps} />
+        <UserProvider>
+          <Component {...pageProps} />
+        </UserProvider>
       </ThemeProvider>
     </>
   );
