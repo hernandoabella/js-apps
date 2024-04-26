117dc7b34624f220ed740092f1996b7eda569a86
Hernando Abella <hernandoabella@gmail.com>
2024-04-26 02:59:23 -0500

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import { FaGithub } from "react-icons/fa";
 import Image from "next/image";
 
 import dynamic from "next/dynamic";
-import { useUser } from '@auth0/nextjs-auth0/client';
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 const Header: React.FC = () => {
   const { theme, setTheme } = useTheme();
@@ -27,17 +27,43 @@ const Header: React.FC = () => {
     localStorage.setItem("theme", theme ?? "light");
   }, [theme]);
 
-  const {user, error, isLoading} = useUser();
+  const { user, error, isLoading } = useUser();
 
-  if(isLoading) return <div>Loading...</div>;
-  if(error) return <div>{error.message}</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
 
-  if(user) {
+  if (user) {
     console.log(user);
     return (
-      <div>
-        Wellcome {user.name}! <a href="/api/auth/logout">Logout</a>
-      </div>
+      <header className="dark:bg-slate-900 dark:text-white">
+        <div className="flex justify-between p-10 md:px-20 font-semibold">
+          <div className="flex items-center">
+            <Link href="/">
+              <div className="flex items-center justify-center select-none">
+                <Image src="/logo.png" width={100} height={100} alt={""} />
+              </div>
+            </Link>
+          </div>
+
+          <nav>
+            <ul className="md:flex flex justify-center items-center">
+              <li className="mr-6 text-slate-400 hover:text-slate-500 dark:hover:text-slate-300">
+                <Link href="/pricing" className="bg-purple-500 p-5 text-white rounded-xl">
+                  PRO
+                </Link>
+              </li>
+
+              <li className="mr-6 text-slate-400 hover:text-slate-500 dark:hover:text-slate-300">
+                <Link href="/api/auth/logout">Logout</Link>
+              </li>
+
+              <li className="border rounded-xl">
+                <Image src="/test-avatar.png" alt="profilepic" width={60} height={60}/>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
     );
   }
 
@@ -47,12 +73,7 @@ const Header: React.FC = () => {
         <div className="flex items-center">
           <Link href="/">
             <div className="flex items-center justify-center select-none">
-              <Image
-                src="/logo.png"
-                width={100}
-                height={100}
-                alt={''}
-              />
+              <Image src="/logo.png" width={100} height={100} alt={""} />
             </div>
           </Link>
         </div>
@@ -60,25 +81,17 @@ const Header: React.FC = () => {
         <nav>
           <ul className="md:flex flex justify-center items-center">
             <li className="mr-6 text-slate-400 hover:text-slate-500 dark:hover:text-slate-300">
-              <Link href="/why-us">
-                Why JS-APPS?
-              </Link>
+              <Link href="/why-us">Why JS-APPS?</Link>
             </li>
             <li className="mr-6 text-slate-400 hover:text-slate-500 dark:hover:text-slate-300">
-              <Link href="/pricing">
-                Pricing
-              </Link>
+              <Link href="/pricing">Pricing</Link>
             </li>
 
             <li className="mr-6 text-slate-400 hover:text-slate-500 dark:hover:text-slate-300">
-              <Link href="/api/auth/login">
-                Sign in
-              </Link>
+              <Link href="/api/auth/login">Sign in</Link>
             </li>
             <li className="bg-blue-500 text-white rounded-full p-2 mr-6 text-slate-400">
-              <Link href="/contact">
-                Contact
-              </Link>
+              <Link href="/contact">Contact</Link>
             </li>
             <li className="mr-3" onClick={handleThemeToggle}>
               <span className="text-sky-500 dark:text-sky-400 cursor-pointer">
@@ -97,7 +110,6 @@ const Header: React.FC = () => {
                 <FaGithub className="hover:text-gray-600 w-5 h-5" />
               </Link>
             </li>
-
           </ul>
         </nav>
       </div>
