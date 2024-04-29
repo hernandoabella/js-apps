7ecfef40a51d0e20d1cac8e94457e4c38270f533
Hernando Abella <hernandoabella@gmail.com>
2024-04-29 00:00:55 -0500

    .

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
-        />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
