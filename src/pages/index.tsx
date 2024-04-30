2e5761de5b0177205ffb0574e946ceb82dcc39b2
Hernando Abella <hernandoabella@gmail.com>
2024-04-29 23:42:46 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
 import About from "../components/About/About";
@@ -7,7 +5,7 @@ import Featured from "../components/Featured/Featured";
 import Testimonials from "../components/Testimonials/Testimonials";
 import Faq from "../components/Faq/Faq";
 import Cta from "../components/Cta/Cta";
-import Projects from "../components/Projects/Projects";
+// import Projects from "../components/Projects/Projects";
 import Footer from "../components/Footer/Footer";
 
 export default function Home() {
@@ -15,7 +13,7 @@ export default function Home() {
     <div>
       <Header />
       <Hero />
-      <About />
+      {/* <About /> */}
       {/* <Featured /> */}
       {/* <Testimonials /> */}
       {/* <Faq /> */}
