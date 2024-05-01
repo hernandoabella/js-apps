e2ad1d471dc482606e99832504d6b322c26908d9
Hernando Abella <hernandoabella@gmail.com>
2024-05-01 16:03:01 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,46 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: '/apps',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=3600',
+                    },
+                ],
+            },
+            {
+                source: '/login',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=3600',
+                    },
+                ],
+            },
+            {
+                source: '/contact',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=3600',
+                    },
+                ],
+            },
+            {
+                source: '/pricing',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=3600',
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 module.exports = nextConfig;
