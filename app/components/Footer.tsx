cdf191013e8738f5d67965f6f677b72d94d1e21c
Hernando Abella <hernandoabella@gmail.com>
2024-06-12 13:33:15 -0500

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,20 +1,9 @@
-import Link from "next/link";
-import React from "react";
+import React from 'react'
 
-function Footer() {
+const Footer = () => {
   return (
-    <footer>
-      <div className="p-10 pt-40 md:px-20 dark:text-white dark:bg-slate-900">
-        <p className="text-center">
-          &copy; 2024 JS-APPS by{" "}
-          <Link href={"https://www.x.com/hernandoabella"} target="_blank">
-            <b>@hernandoabella</b>
-          </Link>
-          .
-        </p>
-      </div>
-    </footer>
-  );
+    <div>&copy; 2024 JS-APPS by{" "}Hernando Abella</div>
+  )
 }
 
-export default Footer;
+export default Footer
