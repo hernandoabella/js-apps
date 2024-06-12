fd87baaca202c791e0e8207d91effb8839b3a121
Hernando Abella <hernandoabella@gmail.com>
2024-06-12 13:17:51 -0500

    Create Footer.tsx

---
+++ app/components/Footer.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="p-10 pt-40 md:px-20 dark:text-white dark:bg-slate-900">
+        <p className="text-center">
+          &copy; 2024 JS-APPS by{" "}
+          <Link href={"https://www.x.com/hernandoabella"} target="_blank">
+            <b>@hernandoabella</b>
+          </Link>
+          .
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
