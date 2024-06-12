e4a354387f7e6eff3129dfc6d929ff29708bf4a5
Hernando Abella <hernandoabella@gmail.com>
2024-06-12 14:18:17 -0500

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React from 'react'
 
 const Header = () => {
-  return <div>Header</div>;
-};
+  return (
+    <div>Header</div>
+  )
+}
 
-export default Header;
+export default Header
