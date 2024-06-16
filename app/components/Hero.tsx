a7d9ba799c488199e1ebc613f32629381d097fd3
Hernando Abella <hernandoabella@gmail.com>
2024-06-16 02:51:47 -0500

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () => {
   return (
     <div>
-      <div className="px-10">
+      <div className="px-10 pt-40">
         <h1 className="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight text-center">
           Top{" "}
           <span className="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-purple-400 to-sky-500 lg:inline">
@@ -11,9 +11,8 @@ const Hero = () => {
           </span>{" "}
           to level up your skills
         </h1>
-        <p className="px-0 mb-8 text-lg text-gray-600 md:text-xl lg:px-24">
-          Explore a variety of JavaScript applications across five difficulty
-          levels. Practice and progress from beginner to expert, refining your
+        <p className="text-center px-0 mb-8 text-lg text-gray-600 md:text-xl lg:px-24">
+          Practice and progress from beginner to expert, refining your
           programming abilities.
         </p>
       </div>
