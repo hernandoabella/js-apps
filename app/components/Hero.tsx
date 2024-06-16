abf98a7f7faf5105deb9799cd0a10937fc96fce1
Hernando Abella <hernandoabella@gmail.com>
2024-06-16 02:41:59 -0500

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div>
+      <div className="px-10">
+        <h1 className="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight text-center">
+          Top{" "}
+          <span className="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-purple-400 to-sky-500 lg:inline">
+            JavaScript Projects
+          </span>{" "}
+          to level up your skills
+        </h1>
+        <p className="px-0 mb-8 text-lg text-gray-600 md:text-xl lg:px-24">
+          Explore a variety of JavaScript applications across five difficulty
+          levels. Practice and progress from beginner to expert, refining your
+          programming abilities.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
