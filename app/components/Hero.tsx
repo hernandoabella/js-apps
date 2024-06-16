d536888e314af47a43ee6791dc0c9aa38fefe93b
Hernando Abella <hernandoabella@gmail.com>
2024-06-16 03:06:38 -0500

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="px-10 pt-40">
         <h1 className="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight text-center">
           Top{" "}
-          <span className="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-purple-400 to-sky-500 lg:inline">
+          <span className="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-red-400 to-sky-500 lg:inline">
             JavaScript Projects
           </span>{" "}
           to level up your skills
