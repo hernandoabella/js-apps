0393abcbb263043856fbe76da8cbf395890c238b
Hernando Abella <hernandoabella@gmail.com>
2024-06-20 00:45:33 -0500

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
         <nav className="bg-white dark:bg-black fixed w-full z-20 top-0 start-0">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a
-              href="https://flowbite.com/"
+              href="/"
               className="flex items-center space-x-3 rtl:space-x-reverse"
             >
               {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
