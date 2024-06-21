96288be0255ba75f93b5146b4fc0781b36f08513
Hernando Abella <hernandoabella@gmail.com>
2024-06-20 23:43:48 -0500

    Update page.tsx

--- app/apps/random-number/page.tsx
+++ app/apps/random-number/page.tsx
@@ -131,8 +131,8 @@ const ProjectDetail = () => {
         </div>
       </div>
 
-      <div className="bg-sky-500 p-5 rounded-b-xl flex gap-5">
-        <div>
+      <div className="bg-sky-500 p-5 rounded-b-xl flex justify-between gap-5">
+        <div className="">
           <div>
             <h2 className="text-2xl font-bold mb-2">Random Number Generator</h2>
           </div>
@@ -140,31 +140,33 @@ const ProjectDetail = () => {
             <p>This App allows you to generate a random number.</p>
           </div>
         </div>
-        <div className="mb-2">
-          <a
-            href="https://github.com/your-repo"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500"
-            download
-          >
-            <button type="button" className="button2">
-              <span className="button__text">Download</span>
-              <span className="button__icon">
-                <svg
-                  className="svg"
-                  data-name="Layer 2"
-                  id="bdd05811-e15d-428c-bb53-8661459f9307"
-                  viewBox="0 0 35 35"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path>
-                  <path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path>
-                  <path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path>
-                </svg>
-              </span>
-            </button>
-          </a>
+        <div className="flex items-center">
+          <div className="mb-2">
+            <a
+              href="https://github.com/your-repo"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500"
+              download
+            >
+              <button type="button" className="button2">
+                <span className="button__text">Download</span>
+                <span className="button__icon">
+                  <svg
+                    className="svg"
+                    data-name="Layer 2"
+                    id="bdd05811-e15d-428c-bb53-8661459f9307"
+                    viewBox="0 0 35 35"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path>
+                    <path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path>
+                    <path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path>
+                  </svg>
+                </span>
+              </button>
+            </a>
+          </div>
         </div>
       </div>
     </div>
