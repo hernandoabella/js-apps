c778bb3faec92c19ec2a8660f0526984ec184974
Hernando Abella <hernandoabella@gmail.com>
2024-06-21 02:22:34 -0500

    Update AppDetail.tsx

--- app/components/AppDetail.tsx
+++ app/components/AppDetail.tsx
@@ -1,41 +1,37 @@
 // components/AppDetail.tsx
-import { useState, useEffect, useRef } from "react";
-import {
-  FaHtml5,
-  FaCss3,
-  FaArrowCircleLeft,
-  FaJs,
-  FaCopy,
-  FaGithub,
-  FaDownload,
-} from "react-icons/fa";
+
+import React, { useState, useEffect, useRef } from "react";
+import { FaHtml5, FaCss3, FaArrowCircleLeft, FaJs, FaCopy, FaDownload, FaGithub } from "react-icons/fa";
 import Link from "next/link";
 import hljs from "highlight.js/lib/core";
-import html from "highlight.js/lib/languages/xml";
-import css from "highlight.js/lib/languages/css";
-import javascript from "highlight.js/lib/languages/javascript";
-import "highlight.js/styles/github-dark.css";
+import html from "highlight.js/lib/languages/xml"; // Importa el lenguaje HTML/XML
+import css from "highlight.js/lib/languages/css"; // Importa el lenguaje CSS
+import javascript from "highlight.js/lib/languages/javascript"; // Importa el lenguaje JavaScript
 
-// Register languages with highlight.js
 hljs.registerLanguage("html", html);
 hljs.registerLanguage("css", css);
 hljs.registerLanguage("javascript", javascript);
-
-interface Props {
-  name: string;
-  iframeSrc: string;
-}
 
 const codeSnippets = {
   html: `<pre><code class="language-html hljs">&lt;div&gt;Hello World&lt;/div&gt;</code></pre>`,
   css: `<pre><code class="language-css hljs">div {
     color: red;
 }</code></pre>`,
-  js: `
-    <pre><code class="language-javascript hljs">console.log('Hello, World!');</code></pre>`,
+  js: `<pre><code class="language-javascript hljs">console.log('Hello, World!');</code></pre>`,
 };
 
-const AppDetail: React.FC<Props> = ({ name, iframeSrc }) => {
+interface AppDetailProps {
+  app: {
+    name: string;
+    path: string;
+    iframeSrc: string;
+    level: number;
+    githubLink: string;
+    downloadLink: string;
+  };
+}
+
+const AppDetail: React.FC<AppDetailProps> = ({ app }) => {
   const [codeType, setCodeType] = useState<"html" | "css" | "js">("html");
   const codeRef = useRef<HTMLDivElement | null>(null);
 
@@ -59,9 +55,7 @@ const AppDetail: React.FC<Props> = ({ name, iframeSrc }) => {
         })
         .catch((err) => {
           console.error("Could not copy text: ", err);
-          alert(
-            "Failed to copy code snippet. Please copy it manually."
-          );
+          alert("Failed to copy code snippet. Please copy it manually.");
         });
     } else {
       const textarea = document.createElement("textarea");
@@ -82,7 +76,7 @@ const AppDetail: React.FC<Props> = ({ name, iframeSrc }) => {
       <div className="lg:w-9/12">
         <div className="lg:mx-20 lg:flex-col">
           <div className="bg-sky-500 p-5 rounded-t-xl">
-            <Link href="/">
+            <Link href="/dashboard">
               <div className="inline-flex items-center bg-sky-800 hover:bg-sky-700 text-white p-2 rounded-md">
                 <FaArrowCircleLeft className="mr-2" /> Go back
               </div>
@@ -92,8 +86,8 @@ const AppDetail: React.FC<Props> = ({ name, iframeSrc }) => {
           <div className="flex flex-col md:flex-row">
             <div className="md:w-1/2 bg-white p-2">
               <iframe
-                title="project-iframe"
-                src={iframeSrc}
+                title={app.name}
+                src={app.iframeSrc}
                 className="w-full h-96 border-none"
               ></iframe>
             </div>
@@ -146,16 +140,18 @@ const AppDetail: React.FC<Props> = ({ name, iframeSrc }) => {
           <div className="bg-sky-500 p-5 rounded-b-xl md:flex justify-between gap-5">
             <div className="">
               <div>
-                <h2 className="text-2xl font-bold mb-2">{name}</h2>
+                <h2 className="text-2xl font-bold mb-2">
+                  {app.name}
+                </h2>
               </div>
               <div className="mb-2">
-                <p>This app allows you to generate a random number.</p>
+                <p>This app allows you to {app.name.toLowerCase()}.</p>
               </div>
             </div>
             <div className="flex items-center justify-center gap-5">
               <div className="pt-5 md:pt-0">
                 <a
-                  href={`https://github.com/hernandoabella/${name.toLowerCase().replace(/\s+/g, "-")}.zip`}
+                  href={app.downloadLink}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="text-blue-500"
@@ -171,7 +167,7 @@ const AppDetail: React.FC<Props> = ({ name, iframeSrc }) => {
               </div>
               <div className="pt-5 md:pt-0">
                 <a
-                  href={`https://github.com/hernandoabella/${name.toLowerCase().replace(/\s+/g, "-")}`}
+                  href={app.githubLink}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="text-blue-500"
