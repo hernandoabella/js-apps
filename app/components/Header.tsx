0cc774518512ac025ea761c3a1fcbc5be322779a
Hernando Abella <hernandoabella@gmail.com>
2024-10-18 01:15:31 -0500

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaGithubAlt } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const Header = () => {
   return (
@@ -11,10 +11,9 @@ const Header = () => {
               href="/"
               className="flex items-center space-x-3 rtl:space-x-reverse"
             >
-              {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
-              <span className="text-3xl dark:text-white font-bold">
-                JS-APPS <sup className="text-sm">V2</sup>
-              </span>
+              <div className="text-3xl dark:text-white font-bold">
+                <span className="text-cyan-400">JS-</span>APPS
+              </div>
             </a>
             <div className="items-center gap-5 flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
               <a
@@ -26,7 +25,7 @@ const Header = () => {
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 24">
                     <path d="m18 0 8 12 10-8-4 20H4L0 4l10 8 8-12z"></path>
                   </svg>
-                  Download All Apps
+                  Get All Access
                 </button>
               </a>
 
@@ -34,74 +33,9 @@ const Header = () => {
                 href="https://www.github.com/hernandoabella/js-apps"
                 className="text-3xl text-gray-500 hover:text-gray-400"
               >
-                <FaGithubAlt />
+                <FaGithub />
               </a>
-
-              {/* <button
-                data-collapse-toggle="navbar-sticky"
-                type="button"
-                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                aria-controls="navbar-sticky"
-                aria-expanded="false"
-              >
-                <span className="sr-only">Open main menu</span>
-                <svg
-                  className="w-5 h-5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 17 14"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 1h15M1 7h15M1 13h15"
-                  />
-                </svg>
-              </button> */}
             </div>
-            {/* <div
-              className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-              id="navbar-sticky"
-            >
-              <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-black md:dark:bg-black dark:border-gray-700">
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-white bg-sky-500 rounded md:bg-transparent md:text-sky-500 md:p-0 md:dark:text-sky-500"
-                    aria-current="page"
-                  >
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    About
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Services
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div> */}
           </div>
         </nav>
       </div>
