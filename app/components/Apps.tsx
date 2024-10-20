3c58028e2c56a41ea9ebf6f73fe81c0689d513b8
Hernando Abella <hernandoabella@gmail.com>
2024-10-19 23:44:56 -0500

    Update Apps.tsx

--- app/components/Apps.tsx
+++ app/components/Apps.tsx
@@ -3,6 +3,14 @@ import Link from "next/link";
 import { FaStar } from "react-icons/fa";
 import { apps, App } from "@/app/appData";
 
+const levelIcons: { [key: number]: string } = {
+  1: "/icons/rookie.svg", 
+  2: "/icons/novice.svg", 
+  3: "/icons/pro.svg",    
+  4: "/icons/master.svg", 
+  5: "/icons/ninja.svg",  
+};
+
 const Stars: React.FC<{ level: number }> = ({ level }) => {
   const stars = Array.from({ length: level }, (_, i) => (
     <FaStar key={i} className="text-yellow-500" />
@@ -29,15 +37,14 @@ const Apps: React.FC = () => {
 
   return (
     <div className="p-10 mt-20 flex flex-col items-center">
-      {" "}
-      {/* Centrado del contenedor */}
       {Object.keys(appsByLevel).map((level) => (
         <div key={level} className="mb-10 text-center">
-          {" "}
-          {/* Centrado del texto */}
           <h2 className="text-2xl font-bold mb-2 flex items-center justify-center">
-            {" "}
-            {/* Centrado del encabezado */}
+            <img
+              src={levelIcons[parseInt(level, 10)]}
+              alt={levelDescriptions[parseInt(level, 10)].split(":")[0]}
+              className="w-8 h-8 mr-2" 
+            />
             <span className="mr-2">
               {levelDescriptions[parseInt(level, 10)].split(":")[0]}:
             </span>
@@ -47,8 +54,6 @@ const Apps: React.FC = () => {
             {levelDescriptions[parseInt(level, 10)].split(":")[1]}
           </p>
           <div className="flex flex-col gap-4 justify-center items-center">
-            {" "}
-            {/* Centrado de los círculos */}
             {appsByLevel[parseInt(level, 10)].map((app, index) => (
               <div key={app.name} className="relative">
                 <Link href={app.path}>
@@ -57,18 +62,11 @@ const Apps: React.FC = () => {
                     style={{ zIndex: 10 }}
                   />
                 </Link>
-                <div>
-                  <div
-                    className={` flex font-semibold justify-center items-center w-24 h-24 rounded-full shadow-lg dark:bg-[#404040] duration-300 
-                  ${
-                    index % 2 === 0
-                      ? "transform translate-x-10"
-                      : "transform -translate-x-10"
-                  }`} 
-                  >
-                    <div className="text-center h-10 flex justify-center items-center">
-                      {app.name}
-                    </div>
+                <div className={`border-2 border-yellow-500 flex font-semibold justify-center items-center w-24 h-24 rounded-full shadow-lg dark:bg-[#404040] duration-300 
+                  ${index % 2 === 0 ? "transform translate-x-8" : "transform -translate-x-8"}`}
+                >
+                  <div className="text-center h-10 flex justify-center items-center">
+                    {app.name}
                   </div>
                 </div>
               </div>
