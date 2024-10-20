a9d61cdfaf20652d253de32e0cd1e77a64daeeb2
Hernando Abella <hernandoabella@gmail.com>
2024-10-19 23:10:51 -0500

    Update Apps.tsx

--- app/components/Apps.tsx
+++ app/components/Apps.tsx
@@ -11,12 +11,13 @@ const Stars: React.FC<{ level: number }> = ({ level }) => {
 };
 
 const levelDescriptions: { [key: number]: string } = {
-  1: "Entry Level: For beginners, covers JavaScript basics.",
-  2: "Junior: Enhance skills with some prior programming experience.",
-  3: "Mid: Intermediate stage, refining skills and learning new concepts.",
-  4: "Senior: Experienced developers seeking a challenge.",
-  5: "Expert: For advanced programmers looking for a greater challenge.",
+  1: "Rookie: Just starting out, learning the basics of coding and JavaScript.",
+  2: "Novice: Gaining confidence, enhancing skills, and tackling simple projects.",
+  3: "Pro: Solid understanding of coding concepts, building real-world applications.",
+  4: "Master: Skilled developers mastering complex challenges and leading projects.",
+  5: "Ninja: Advanced programmers, innovating and pushing boundaries in tech.",
 };
+
 
 const Apps: React.FC = () => {
   const appsByLevel: { [key: number]: App[] } = apps.reduce((acc, app) => {
@@ -28,10 +29,10 @@ const Apps: React.FC = () => {
   }, {} as { [key: number]: App[] });
 
   return (
-    <div className="p-10 mt-20">
+    <div className="p-10 mt-20 flex flex-col items-center"> {/* Centrado del contenedor */}
       {Object.keys(appsByLevel).map((level) => (
-        <div key={level} className="mb-10">
-          <h2 className="text-2xl font-bold mb-2 flex items-center">
+        <div key={level} className="mb-10 text-center"> {/* Centrado del texto */}
+          <h2 className="text-2xl font-bold mb-2 flex items-center justify-center"> {/* Centrado del encabezado */}
             <span className="mr-2">
               {levelDescriptions[parseInt(level, 10)].split(":")[0]}:
             </span>
@@ -40,25 +41,23 @@ const Apps: React.FC = () => {
           <p className="text-lg mb-4">
             {levelDescriptions[parseInt(level, 10)].split(":")[1]}
           </p>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
-            {appsByLevel[parseInt(level, 10)].map((app) => (
+          <div className="flex flex-col gap-4 justify-center items-center"> {/* Centrado de los círculos */}
+            {appsByLevel[parseInt(level, 10)].map((app, index) => (
               <div key={app.name} className="relative">
                 <Link href={app.path}>
                   <div
-                    className="block  absolute inset-0"
+                    className="block absolute inset-0"
                     style={{ zIndex: 10 }}
                   />
                 </Link>
-                <div className=" rounded-xl bg-white/20 shadow-lg ">
-                  {app.ProjectComponent && (
-                    <div className="w-full h-72 flex justify-center items-center">
-                      <app.ProjectComponent />
-                    </div>
-                    
-                  )}
-                  
+                <div
+                  className={`flex font-semibold justify-center items-center w-24 h-24 rounded-full shadow-lg dark:bg-[#404040] duration-300 
+                  ${index % 2 === 0 ? 'transform translate-x-10' : 'transform -translate-x-10'}`} // Desplazamiento alternativo
+                >
+                  <div className="text-center h-10 flex justify-center items-center">
+                    {app.name}
+                  </div>
                 </div>
-                
               </div>
             ))}
           </div>
