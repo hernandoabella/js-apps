c8aeba5958b73702643c783038ba69229b370e16
Hernando Abella <hernandoabella@gmail.com>
2024-10-20 23:07:53 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head></head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
