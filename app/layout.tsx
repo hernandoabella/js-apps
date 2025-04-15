ddc0ed3bb6c876ccd773339bc2fc8955679ac604
Hernando Abella <hernandoabella@gmail.com>
2025-04-14 23:15:53 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "JS-APPS",
-  description: "Learn JavaScript by building projects!",
+  description: "Learn JavaScript by building real-life projects!",
 };
 
 export default function RootLayout({
