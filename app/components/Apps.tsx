6d72f97dc778c972a9aa3356d9170f3ebccd64c8
Hernando Abella <hernandoabella@gmail.com>
2025-04-16 23:39:28 -0500

    Update Apps.tsx

--- app/components/Apps.tsx
+++ app/components/Apps.tsx
@@ -64,7 +64,7 @@ const Apps: React.FC = () => {
               >
                 <Link href={app.path}>
 
-                  <button className="cursor-pointer transition-all bg-yellow-400 text-black px-6 py-2 rounded-lg
+                  <button className="cursor-pointer transition-all bg-yellow-400 text-[#8f6712] px-6 py-2 rounded-lg
 border-yellow-600
 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
 active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">{/* Project button with the icon */}
