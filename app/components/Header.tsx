adb6d70a9c019e12838babaed0ac5691cf4d6117
Hernando Abella <hernandoabella@gmail.com>
2025-04-29 11:00:26 -0500

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -10,15 +10,10 @@ const Header = () => {
         <div className="mx-auto max-w-7xl flex flex-wrap items-center justify-between py-4 px-4 md:px-10">
           <a
             href="/"
-            className="flex items-center space-x-3 rtl:space-x-reverse"
+            className="flex items-center space-x-3"
           >
-            <div className="logo">
-            <Image
-                src="/logo.png"
-                alt="logo"
-                width={180}
-                height={40}
-              />
+            <div className="text-3xl font-bold font-[var(--font-squada-one)] tracking-widest hover:text-yellow-300 transition duration-300">
+              <span className="text-yellow-500">&lt;/</span>JSAPPS
             </div>
           </a>
           <div className="flex items-center gap-5 md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
@@ -30,6 +25,36 @@ const Header = () => {
             </a>
 
             <ModeToggle />
+
+            <div className="relative inline-flex items-center justify-center gap-4 group">
+              <div
+                className="absolute inset-0 duration-1000 opacity-60 transitiona-all bg-gradient-to-r from-indigo-500 via-pink-500 to-yellow-400 rounded-xl blur-lg filter group-hover:opacity-100 group-hover:duration-200"
+              ></div>
+              <a
+                role="button"
+                className="group relative inline-flex items-center justify-center text-base rounded-xl bg-gray-900 px-8 py-3 font-semibold text-white transition-all duration-200 hover:bg-gray-800 hover:shadow-lg hover:-translate-y-0.5 hover:shadow-gray-600/30"
+                title="payment"
+                href="https://hernandoabella.gumroad.com/l/hoocc"
+              >Get All Apps<svg
+                aria-hidden="true"
+                viewBox="0 0 10 10"
+                height="10"
+                width="10"
+                fill="none"
+                className="mt-0.5 ml-2 -mr-1 stroke-white stroke-2"
+              >
+                  <path
+                    d="M0 5h7"
+                    className="transition opacity-0 group-hover:opacity-100"
+                  ></path>
+                  <path
+                    d="M1 1l4 4-4 4"
+                    className="transition group-hover:translate-x-[3px]"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+
 
             {/* <a
               rel="noopener"
