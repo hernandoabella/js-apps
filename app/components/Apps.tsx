410f6484dec75774819cf6da1858e62f4a575a72
Hernando Abella <hernandoabella@gmail.com>
2025-04-29 11:47:53 -0500

    Update Apps.tsx

--- app/components/Apps.tsx
+++ app/components/Apps.tsx
@@ -39,55 +39,69 @@ const Apps: React.FC = () => {
     <div className="p-10 mt-20 flex flex-col items-center">
       {Object.keys(appsByLevel).map((level) => (
         <div className="my-10">
-          
-          <div key={level} className="text-center rounded-xl">
-          <h2 className="text-2xl font-bold mb-2 flex items-center justify-center">
-            <img
-              src={levelIcons[parseInt(level, 10)]}
-              alt={levelDescriptions[parseInt(level, 10)].split(":")[0]}
-              className="w-20 h-20 m-auto"
-            />
-            <div>
-              <span className="mr-2">
-                {levelDescriptions[parseInt(level, 10)].split(":")[0]}:
-              </span>
-              <Stars level={parseInt(level, 10)} />
-            </div>
-          </h2>
-          <p className="text-lg mb-4 text-gray-500">
-            {levelDescriptions[parseInt(level, 10)].split(":")[1]}
-          </p>
-          <div className="flex flex-col gap-4 justify-center items-center">
-            {appsByLevel[parseInt(level, 10)].map((app, index) => (
-              <div
-                key={app.name}
-                className={`${index === 0 ? "translate-x-0" : index % 2 === 0 ? "translate-x-10" : "-translate-x-10"
-                  }`}
-              >
-                <Link href={app.path}>
 
-                  <button className="cursor-pointer transition-all bg-yellow-400 text-[#8f6712] px-6 py-2 rounded-lg
+          <div className="relative inline-flex items-center justify-center gap-4 group">
+            <div
+              className="absolute inset-0 duration-1000 opacity-60 transitiona-all bg-gradient-to-r from-indigo-500 via-pink-500 to-yellow-400 rounded-xl blur-lg filter group-hover:opacity-100 group-hover:duration-200"
+            ></div>
+            <a
+              role="button"
+              className="group relative inline-flex items-center justify-center text-base rounded-xl bg-gray-900 px-8 py-3 font-semibold text-white transition-all duration-200 hover:bg-gray-800 hover:shadow-lg hover:-translate-y-0.5 hover:shadow-gray-600/30"
+              title="payment"
+              href=""
+            >
+            <div key={level} className="text-center rounded-xl">
+            <h2 className="text-2xl font-bold mb-2 flex items-center justify-center">
+              <img
+                src={levelIcons[parseInt(level, 10)]}
+                alt={levelDescriptions[parseInt(level, 10)].split(":")[0]}
+                className="w-20 h-20 m-auto"
+              />
+              <div>
+                <span className="mr-2">
+                  {levelDescriptions[parseInt(level, 10)].split(":")[0]}:
+                </span>
+                <Stars level={parseInt(level, 10)} />
+              </div>
+            </h2>
+            <p className="text-lg mb-4 text-gray-500">
+              {levelDescriptions[parseInt(level, 10)].split(":")[1]}
+            </p>
+            <div className="flex flex-col gap-4 justify-center items-center">
+              {appsByLevel[parseInt(level, 10)].map((app, index) => (
+                <div
+                  key={app.name}
+                  className={`${index === 0 ? "translate-x-0" : index % 2 === 0 ? "translate-x-10" : "-translate-x-10"
+                    }`}
+                >
+                  <Link href={app.path}>
+
+                    <button className="cursor-pointer transition-all bg-yellow-400 text-[#8f6712] px-6 py-2 rounded-lg
 border-yellow-600
 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
 active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">{/* Project button with the icon */}
-                    <span className="text-center">
-                      {/* Add the SVG icon here */}
-                      <img
-                        src={app.icon}
-                        alt={app.name}
-                        className="w-10 h-10 mx-auto my-2"
-                      />
+                      <span className="text-center">
+                        {/* Add the SVG icon here */}
+                        <img
+                          src={app.icon}
+                          alt={app.name}
+                          className="w-10 h-10 mx-auto my-2"
+                        />
 
-                    </span>
-                    <span className="absolute inset-0 overflow-hidden rounded-lg">
-                      <span className="absolute left-0 aspect-square w-full origin-center -translate-x-full rounded-full bg-yellow-400 transition-all duration-500 group-hover:-translate-x-0 group-hover:scale-150"></span>
-                    </span><span className="relative z-10">{app.name}</span><span className="absolute inset-0 overflow-hidden rounded-lg"><span className="absolute left-0 aspect-square w-full origin-center -translate-x-full rounded-full bg-yellow-400 transition-all duration-500 group-hover:-translate-x-0 group-hover:scale-150"></span></span></button>
-                </Link>
+                      </span>
+                      <span className="absolute inset-0 overflow-hidden rounded-lg">
+                        <span className="absolute left-0 aspect-square w-full origin-center -translate-x-full rounded-full bg-yellow-400 transition-all duration-500 group-hover:-translate-x-0 group-hover:scale-150"></span>
+                      </span><span className="relative z-10">{app.name}</span><span className="absolute inset-0 overflow-hidden rounded-lg"><span className="absolute left-0 aspect-square w-full origin-center -translate-x-full rounded-full bg-yellow-400 transition-all duration-500 group-hover:-translate-x-0 group-hover:scale-150"></span></span></button>
+                  </Link>
 
-              </div>
-            ))}
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+            </a>
+          </div>
+
+          
         </div>
       ))}
     </div>
