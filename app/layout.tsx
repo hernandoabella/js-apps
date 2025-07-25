42f2979a9f88b5635859d6cdc40e8462ccd383d8
Hernando Abella <hernandoabella@gmail.com>
2025-07-25 15:37:25 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body className={`${inter.className} ${squada.variable}`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
