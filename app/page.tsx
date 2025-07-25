ee74280dfdb1a134125276bc314d2539b7c3f3f4
Hernando Abella <hernandoabella@gmail.com>
2025-07-25 16:33:37 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import LeftSideBar from "@/app/components/LeftSideBar";
+import RightSideBar from "./components/RightSideBar";
 import Apps from "@/app/components/Apps";
 import Footer from "@/app/components/Footer";
+
 
 export default function Home() {
   return (
     <div>
       <div>
+        <RightSideBar />
         <LeftSideBar />
         <Apps />
         <Footer />
