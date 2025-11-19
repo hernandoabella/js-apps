18ee88e6c0aa8dffdacc6ede5f51775e411edd22
Hernando Abella <hernandoabella@gmail.com>
2025-11-18 23:13:40 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 import Apps from "@/app/components/Apps";
-import Footer from "@/app/components/Footer";
-
 
 export default function Home() {
   return (
     <div>
-      <div>
-        <Apps />
-        {/* <Footer /> */}
-      </div>
+      <Apps />
     </div>
   );
 }
